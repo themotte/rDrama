ade99c8f525f69d0a461074197adcfd99e8f4b18
Aevann1 <Aevann1@protonmail.com>
2021-07-21 13:04:04 +0200

    sneed

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -731,26 +731,6 @@ $(document).ready(function() {
 		});
 });
 
-//yt embed
-
-function getId(url) {
-	var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-	var match = url.match(regExp);
-
-	if (match && match[2].length == 11) {
-		return match[2];
-	} else {
-		return 'error';
-	}
-}
-
-var myUrl = $('#embedURL').text();
-
-myId = getId(myUrl);
-
-$('#ytEmbed').html('<iframe width="100%" height="475" src="//www.youtube.com/embed/' + myId + '" frameborder="0" allowfullscreen></iframe>');
-
-
 // Text Formatting
 
 // Bold Text
