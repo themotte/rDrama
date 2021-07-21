c6e7dd7aa39fd21f13cf2dff565a580fc07dfd7f
Aevann1 <Aevann1@protonmail.com>
2021-07-21 13:25:13 +0200

    sneed

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -641,45 +641,6 @@ $('#password-register').on('input', function () {
 
 });
 
-// Check username length, special chars
-
-$('#username-register').on('input', function () {
-
-	var charCount = document.getElementById("username-register").value;
-	var id = document.getElementById("usernameHelpRegister");
-	var successID = document.getElementById("usernameHelpSuccess");
-
-	var ruqqusAPI = '/api/is_available/' + charCount;
-
-	if (charCount.length >= 3) {
-
-		$.getJSON(ruqqusAPI, function(result) {
-			$.each(result, function(i, field) {
-				if (field == false) {
-					id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username already taken :(';
-				}
-			});
-		});
-
-	}
-
-	if (!/[^a-zA-Z0-9_\-$]/.test(charCount)) {
-		// Change alert text
-		id.innerHTML = '<span class="form-text font-weight-bold text-success mt-1">Username is a-okay!';
-
-		if (charCount.length < 3) {
-			id.innerHTML = '<span class="form-text font-weight-bold text-muted mt-1">Username must be at least 3 characters long.';
-		}
-		else if (charCount.length > 25) {
-			id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username must be 25 characters or less.';
-		}
-	}
-	else {
-		id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">No special characters or spaces allowed.</span>';
-	};
-
-});
-
 // Search Icon
 // Change navbar search icon when form is in focus, active states
 
