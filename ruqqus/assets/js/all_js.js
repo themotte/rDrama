5fd2f723a540e65edd2bdbecc5b1fe2cd2a48b3b
Aevann1 <Aevann1@protonmail.com>
2021-07-21 13:34:18 +0200

    sneed

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -440,113 +440,6 @@ function emailVerifyText() {
 
 }
 
-//flagging
-// Flag Comment
-
-report_commentModal = function(id, author) {
-
-	document.getElementById("comment-author").textContent = author;
-
-	//offtopic.disabled=true;
-
-	document.getElementById("reportCommentButton").onclick = function() {
-
-		this.innerHTML='<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Reporting comment';
-		this.disabled = true;
-		post('/api/flag/comment/' + id,
-			callback = function() {
-
-				document.getElementById("reportCommentFormBefore").classList.add('d-none');
-				document.getElementById("reportCommentFormAfter").classList.remove('d-none');
-			}
-			)
-	}
-
-};
-
-$('#reportCommentModal').on('hidden.bs.modal', function () {
-
-	var button = document.getElementById("reportCommentButton");
-
-	var beforeModal = document.getElementById("reportCommentFormBefore");
-	var afterModal = document.getElementById("reportCommentFormAfter");
-
-	button.innerHTML='Report comment';
-	button.disabled= false;
-	afterModal.classList.add('d-none');
-
-	if ( beforeModal.classList.contains('d-none') ) {
-		beforeModal.classList.remove('d-none');
-	}
-
-});
-
-
-// Flag Submission
-
-report_postModal = function(id, author, board) {
-
-	document.getElementById("post-author").textContent = author;
-
-	selectbox=document.getElementById('report-type-dropdown');
-	selectbox.value='reason_not_selected';
-
-	submitbutton=document.getElementById("reportPostButton");
-	submitbutton.disabled=true;
-
-	submitbutton.onclick = function() {
-
-		this.innerHTML='<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Reporting post';
-		this.disabled = true;
-
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", '/api/flag/post/'+id, true);
-		var form = new FormData()
-		form.append("formkey", formkey());
-
-		dropdown=document.getElementById("report-type-dropdown");
-		form.append("report_type", dropdown.options[dropdown.selectedIndex].value);
-
-		xhr.withCredentials=true;
-
-		xhr.onload=function() {
-			document.getElementById("reportPostFormBefore").classList.add('d-none');
-			document.getElementById("reportPostFormAfter").classList.remove('d-none');
-		};
-
-		xhr.onerror=function(){alert(errortext)};
-		xhr.send(form);
-
-	}
-};
-
-$('#reportPostModal').on('hidden.bs.modal', function () {
-
-	var button = document.getElementById("reportPostButton");
-
-	var beforeModal = document.getElementById("reportPostFormBefore");
-	var afterModal = document.getElementById("reportPostFormAfter");
-
-	button.innerHTML='Report post';
-	button.disabled= false;
-
-	afterModal.classList.add('d-none');
-
-	if ( beforeModal.classList.contains('d-none') ) {
-		beforeModal.classList.remove('d-none');
-	}
-
-});
-
-//enlarge thumbs
-// Enlarge submissionlisting thumbnail
-
-enlarge_thumb = function(post_id) {
-
-	document.getElementById(post_id).classList.toggle("enlarged");
-
-};
-
 //iOS webapp stuff
 
 (function(document,navigator,standalone) {
@@ -621,25 +514,6 @@ function post_toast(url, callback) {
 
 	}
 
-//Signup js
-// Display username and password requirements on input
-
-$('#password-register').on('input', function () {
-
-	var charCount = document.getElementById("password-register").value;
-	var id = document.getElementById("passwordHelpRegister");
-	var successID = document.getElementById("passwordHelpSuccess");
-
-	if (charCount.length >= 8) {
-		id.classList.add("d-none");
-		successID.classList.remove("d-none");
-	}
-	else {
-		id.classList.remove("d-none");
-		successID.classList.add("d-none");
-	};
-
-});
 
 // Search Icon
 // Change navbar search icon when form is in focus, active states
