60b51ebef4689c5482d30741f8b60ef0b10994b5
Aevann1 <Aevann1@protonmail.com>
2021-07-23 17:27:00 +0200

    sneed

--- drama/routes/settings.py
+++ drama/routes/settings.py
@@ -68,7 +68,8 @@ def settings_profile_post(v):
 		if v.animatedname == False:
 			users1, users2 = leaderboard()
 			print(users1)
-			if  v not in users1 and v not in users2: return jsonify({"error": "You must be in the leaderboard to apply animated name!"}), 403
+			print(v)
+			if v not in users1 and v not in users2: return jsonify({"error": "You must be in the leaderboard to apply animated name!"}), 403
 		updated = True
 		v.animatedname = request.values.get("animatedname", None) == 'true'
 
