f675fc670050d88599e5548d1e9e061d1b9cac9b
Aevann1 <Aevann1@protonmail.com>
2021-07-26 03:11:55 +0200

    dfd

--- drama/classes/comment.py
+++ drama/classes/comment.py
@@ -80,7 +80,6 @@ class Comment(Base, Age_times, Scores, Stndrd, Fuzzing):
 	# There is no difference to SQLAlchemy, but they cannot be written to
 	ups = deferred(Column(Integer, server_default=FetchedValue()))
 	downs = deferred(Column(Integer, server_default=FetchedValue()))
-	is_public = deferred(Column(Boolean, server_default=FetchedValue()))
 
 	score = deferred(Column(Integer, server_default=FetchedValue()))
 
