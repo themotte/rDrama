eb42b54e474250109292ac65dfe45a99e7c49acd
Aevann1 <Aevann1@protonmail.com>
2021-07-30 16:50:09 +0200

    fdfd

--- drama/routes/admin.py
+++ drama/routes/admin.py
@@ -25,7 +25,7 @@ def patrons(v):
 	if v and v.is_banned and not v.unban_utc: return render_template("seized.html")
 	if not (v and v.admin_level == 6): abort(404)
 	users = [x for x in g.db.query(User).filter_by(patron = True).all()]
-	return render_template("banned.html", v=v, users=users)
+	return render_template("patrons.html", v=v, users=users)
 
 
 @app.get("/admin/shadowbanned")
