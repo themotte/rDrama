13414d977011f2bf2543b4633a735f4a9a66c602
Aevann1 <Aevann1@protonmail.com>
2021-07-30 17:42:01 +0200

    gfgf

--- drama/classes/award.py
+++ drama/classes/award.py
@@ -1,9 +1,6 @@
-#import requests
-#from os import environ
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
-#from .mix_ins import *
-from drama.__main__ import Base, app
+from drama.__main__ import Base
 
 AWARDS = {
 	"ban": {
