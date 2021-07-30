d48e4228d1d81e81ca9cb1d2a0a9ac0b9541b1a5
Aevann1 <Aevann1@protonmail.com>
2021-07-30 17:14:24 +0200

    dfdfd

--- drama/routes/admin.py
+++ drama/routes/admin.py
@@ -19,11 +19,10 @@ import matplotlib.pyplot as plt
 from drama.__main__ import app, cache
 from .front import frontlist
 
-@app.get("/admin/patrons")
-@auth_required
+@app.get("/patrons")
+@auth_desired
 def patrons(v):
 	if v and v.is_banned and not v.unban_utc: return render_template("seized.html")
-	if not (v and v.admin_level == 6): abort(404)
 	users = [x for x in g.db.query(User).filter(User.patron > 0).order_by(User.patron.desc()).all()]
 	return render_template("patrons.html", v=v, users=users)
 
