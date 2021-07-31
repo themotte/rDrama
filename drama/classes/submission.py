cc85903c4c4edf849500aa4396df7c29bdf6db62
Aevann1 <Aevann1@protonmail.com>
2021-07-31 15:48:50 +0200

    fdfd

--- drama/classes/submission.py
+++ drama/classes/submission.py
@@ -56,7 +56,7 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
 		backref="submissions")
 	domain_ref = Column(Integer, ForeignKey("domains.id"))
 	domain_obj = relationship("Domain")
-	flags = relationship("Flag", lazy="dynamic", primaryjoin="Flag.submission_id==Submission.id")
+	flags = relationship("Flag", lazy="dynamic")
 	is_approved = Column(Integer, ForeignKey("users.id"), default=0)
 	over_18 = Column(Boolean, default=False)
 	author = relationship(
