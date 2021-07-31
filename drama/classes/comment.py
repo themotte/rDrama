46ff30561f358bd530a15298cb4f4a66753aff92
Aevann1 <Aevann1@protonmail.com>
2021-07-31 15:56:34 +0200

    fdfd

--- drama/classes/comment.py
+++ drama/classes/comment.py
@@ -293,7 +293,7 @@ class Comment(Base, Age_times, Scores, Stndrd, Fuzzing):
 	
 	@property
 	@lazy
-	def active_flags(self): return len(self.flags)
+	def active_flags(self): return self.flags.count()
 
 	@property
 	@lazy
