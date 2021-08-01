71d9a60bc994e06f4b51db92407fa8fc3b95d745
Aevann1 <Aevann1@protonmail.com>
2021-08-01 13:10:22 +0200

    fdfd

--- pushcss.sh
+++ pushcss.sh
@@ -1,8 +1,8 @@
-sass ./drama/assets/style/dark.scss D:/#D/drama/assets/style/dark_ff66ac.css
-sass ./drama/assets/style/light.scss D:/#D/drama/assets/style/light_ff66ac.css
-sass ./drama/assets/style/coffee.scss D:/#D/drama/assets/style/coffee_ff66ac.css
-sass ./drama/assets/style/tron.scss D:/#D/drama/assets/style/tron_ff66ac.css
-sass ./drama/assets/style/4chan.scss D:/#D/drama/assets/style/4chan_ff66ac.css
+sass ./drama/assets/style/dark.scss ./drama/assets/style/dark_ff66ac.css
+sass ./drama/assets/style/light.scss ./drama/assets/style/light_ff66ac.css
+sass ./drama/assets/style/coffee.scss ./drama/assets/style/coffee_ff66ac.css
+sass ./drama/assets/style/tron.scss ./drama/assets/style/tron_ff66ac.css
+sass ./drama/assets/style/4chan.scss ./drama/assets/style/4chan_ff66ac.css
 python ./compilecss.py
 git add .
 git commit -m "css"
