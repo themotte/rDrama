903ccd48a48ccac8e3a6c600026c2f838ba039cf
Aevann1 <Aevann1@protonmail.com>
2021-08-02 09:34:22 +0200

    sneed

--- compilecss.py
+++ compilecss.py
@@ -1,4 +1,4 @@
-for theme in ['dark', 'light', 'coffee', 'tron', '4chan']:
+for theme in ['midnight', 'dark', 'light', 'coffee', 'tron', '4chan']:
     with open(f"./drama/assets/style/{theme}_ff66ac.css", encoding='utf-8') as t:
         text = t.read()
         for color in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','62ca56','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58']:
--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git commit -m "sneed"
 git push
 
 apt install ruby-sass
+sass ./drama/assets/style/midnight.scss ./drama/assets/style/midnight_ff66ac.css
 sass ./drama/assets/style/dark.scss ./drama/assets/style/dark_ff66ac.css
 sass ./drama/assets/style/light.scss ./drama/assets/style/light_ff66ac.css
 sass ./drama/assets/style/coffee.scss ./drama/assets/style/coffee_ff66ac.css
