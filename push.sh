7b3b1e7ba271ff6e9333ead8acc5d7fc89490026
Aevann1 <Aevann1@protonmail.com>
2021-08-02 08:45:05 +0200

    gfgf

--- push.sh
+++ push.sh
@@ -3,12 +3,14 @@ git add .
 git commit -m "sneed"
 git push
 
+apt install ruby-sass
 sass ./drama/assets/style/dark.scss ./drama/assets/style/dark_ff66ac.css
 sass ./drama/assets/style/light.scss ./drama/assets/style/light_ff66ac.css
 sass ./drama/assets/style/coffee.scss ./drama/assets/style/coffee_ff66ac.css
 sass ./drama/assets/style/tron.scss ./drama/assets/style/tron_ff66ac.css
 sass ./drama/assets/style/4chan.scss ./drama/assets/style/4chan_ff66ac.css
 python ./compilecss.py
+python3 ./compilecss.py
 git add .
 git commit -m "css"
 git push
