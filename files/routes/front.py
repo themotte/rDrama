b625f85b6112e532e1193113e8e575db1442be9a
Aevann1 <Aevann3@gmail.com>
2021-08-06 14:39:06 +0200

    gtr

--- files/routes/front.py
+++ files/routes/front.py
@@ -128,11 +128,11 @@ def frontlist(v=None, sort="hot", page=1,t="all", ids_only=True, filter_words=''
 	elif sort == "top":
 		time1 = time.time()
 		posts = sorted(posts.all(), key=lambda x: x.score, reverse=True)
-		print(time.time() - time1)
+		print("list +"time.time() - time1)
 	elif sort == "bottom":
 		time1 = time.time()
 		posts = posts.order_by(Submission.sexscore.desc()).all()
-		print(time.time() - time1)
+		print("func " + time.time() - time1)
 	elif sort == "comments":
 		posts = posts.order_by(Submission.comment_count.desc()).all()
 	elif sort == "random":
