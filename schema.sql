b9c28776d0ee73b244d04a2b3b53b859a80f391f
Aevann1 <Aevann3@gmail.com>
2021-08-11 18:00:17 +0000

    schema

--- schema.sql
+++ schema.sql
@@ -1039,6 +1039,7 @@ CREATE TABLE public.users (
     agendaposter boolean,
     agendaposter_expires_utc integer DEFAULT 0,
     resized boolean,
+    animatedname boolean,
     suicide_utc integer,
     post_count integer,
     comment_count integer,
