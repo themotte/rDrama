3c636fa86fcfd1e19d086e32996a8123aab2acce
Aevann1 <Aevann3@gmail.com>
2021-08-19 07:19:02 +0200

    dfdfd

--- files/__main__.py
+++ files/__main__.py
@@ -47,7 +47,8 @@ app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.config["PERMANENT_SESSION_LIFETIME"] = 60 * 60 * 24 * 365
 app.config["SESSION_REFRESH_EACH_REQUEST"] = True
 
-app.config["DEFAULT_THEME"] = environ.get("defaulttheme").strip() + "_" + environ.get("defaultcolor").strip()
+app.config["DEFAULT_COLOR"] = environ.get("DEFAULT_COLOR").strip()
+app.config["DEFAULT_THEME"] = environ.get("DEFAULT_THEME").strip() + "_" + environ.get("DEFAULT_COLOR").strip()
 
 app.config["FORCE_HTTPS"] = int(environ.get("FORCE_HTTPS", 1)) if ("localhost" not in app.config["SERVER_NAME"] and "127.0.0.1" not in app.config["SERVER_NAME"]) else 0
 
