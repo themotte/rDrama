116989a32034ec303085a6c2596d00330087dbf2
Aevann1 <Aevann3@gmail.com>
2021-08-21 14:30:52 +0200

    df

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -563,7 +563,7 @@ def submit_post(v):
 	else:
 		url = ""
 
-	if "i.imgur.com" in url: url = url.replace(".png", "_d.png").replace(".jpg", "_d.jpg").replace(".jpeg", "_d.jpeg") + "?maxwidth=8888"
+	if "i.imgur.com" in url: url = url.replace(".png", "_d.png").replace(".jpg", "_d.jpg").replace(".jpeg", "_d.jpeg") + "?maxwidth=9999"
 	
 	body = request.form.get("body", "")
 	# check for duplicate
