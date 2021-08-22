237ff4c2f24d841a686141d3bf4e0b7e1c1a370b
Aevann1 <Aevann3@gmail.com>
2021-08-22 15:15:10 +0200

    dfdf

--- files/routes/front.py
+++ files/routes/front.py
@@ -283,7 +283,7 @@ def changeloglist(v=None, sort="new", page=1 ,t="all", **kwargs):
 	elif sort == "bottom":
 		posts = sorted(posts.all(), key=lambda x: x.score)
 	elif sort == "comments":
-		posts = sorted(posts.all(), key=lambda x: x.comment_count, reverse=True)
+		posts = posts.order_by(Submission.comment_count.desc()).all()
 	elif sort == "random":
 		posts = posts.all()
 		posts = random.sample(posts, k=len(posts))
