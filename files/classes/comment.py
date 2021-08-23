37dc681186933675dd89917211703fb553ca00f5
Aevann1 <Aevann3@gmail.com>
2021-08-23 20:44:39 +0200

    fdfd

--- files/classes/comment.py
+++ files/classes/comment.py
@@ -116,7 +116,7 @@ class Comment(Base, Age_times, Scores, Stndrd, Fuzzing):
 	@property
 	def replies(self):
 		r = self.__dict__.get("replies", None)
-		if not r and r != []: r = sorted([x for x in self.child_comments if not x.shadowbanned], key=lambda x: x.score, reverse=True)
+		if not r and r != []: r = sorted([x for x in self.child_comments if not x.author.shadowbanned], key=lambda x: x.score, reverse=True)
 		return r
 
 	@replies.setter
