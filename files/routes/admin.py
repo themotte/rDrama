428818a728529954528373c7d4dc439e35f04ec6
Aevann1 <Aevann3@gmail.com>
2021-08-28 14:33:08 +0200

    gf

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -129,10 +129,10 @@ def monthly(v):
 			grant_awards["shit"] = 10
 			grant_awards["gold"] = 10
 			grant_awards["ban"] = 3
-		elif u.patron == 5 or u.patron == 8:
-			grant_awards["shit"] = 10
-			grant_awards["gold"] = 10
-			grant_awards["ban"] = 6
+		# elif u.patron == 5:
+		# 	grant_awards["shit"] = 10
+		# 	grant_awards["gold"] = 10
+		# 	grant_awards["ban"] = 6
 
 
 		for name in grant_awards:
@@ -253,11 +253,11 @@ def badge_grant_post(v):
 			grant_awards["shit"] = 10
 			grant_awards["gold"] = 10
 			grant_awards["ban"] = 3
-		elif badge_id == 25:
-			if user.discord_id: add_role(user, "5")
-			grant_awards["shit"] = 10
-			grant_awards["gold"] = 10
-			grant_awards["ban"] = 6
+		# elif badge_id == 25:
+		# 	if user.discord_id: add_role(user, "5")
+		# 	grant_awards["shit"] = 10
+		# 	grant_awards["gold"] = 10
+		# 	grant_awards["ban"] = 6
 
 		if len(grant_awards):
 
