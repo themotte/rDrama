f793ef8657524abca4eeb19ac76d8a298fb97e65
Aevann1 <Aevann3@gmail.com>
2021-08-31 21:02:00 +0200

    fddf

--- files/routes/discord.py
+++ files/routes/discord.py
@@ -116,6 +116,7 @@ def discord_redirect(v):
 	if x.status_code in [201, 204]:
 
 		if v.id == 1: add_role(v, "shrigma")
+		time.sleep(0.1)
 		if v.admin_level > 0: add_role(v, "admin")
 		else: add_role(v, "newuser")
 		
