7ba30b358a6138682da2c1474114b490f1e7ad00
Aevann1 <Aevann3@gmail.com>
2021-08-31 17:57:24 +0200

    dfdf

--- files/routes/front.py
+++ files/routes/front.py
@@ -24,7 +24,7 @@ def notifications(v):
 	messages = request.args.get('messages', False)
 	posts = request.args.get('posts', False)
 	if messages:
-		if v.admin_level == 6: comments = g.db.query(Comment).filter(Comment.child_comments == [], or_(Comment.author_id==v.id, Comment.sentto==v.id, Comment.sentto==0)).filter(Comment.parent_submission == None).order_by(Comment.created_utc.desc()).offset(25 * (page - 1)).limit(26).all()
+		if v.admin_level == 6: comments = g.db.query(Comment).filter(len(Comment.child_comments) == 0, or_(Comment.author_id==v.id, Comment.sentto==v.id, Comment.sentto==0)).filter(Comment.parent_submission == None).order_by(Comment.created_utc.desc()).offset(25 * (page - 1)).limit(26).all()
 		else: comments = g.db.query(Comment).filter(or_(Comment.author_id==v.id, Comment.sentto==v.id)).filter(Comment.parent_submission == None).order_by(Comment.created_utc.desc()).offset(25 * (page - 1)).limit(26).all()
 		next_exists = (len(comments) == 26)
 		comments = comments[:25]
