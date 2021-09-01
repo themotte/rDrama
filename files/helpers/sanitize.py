5bb78fb58b85b0f41854ae3b138e2ef6257aa0dc
Aevann1 <Aevann3@gmail.com>
2021-09-01 23:09:43 +0200

    vcvc

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -241,10 +241,11 @@ def sanitize(sanitized, noimages=False):
 	for rd in ["https://reddit.com/", "https://new.reddit.com/", "https://www.reddit.com/", "https://redd.it/"]:
 		sanitized = sanitized.replace(rd, "https://old.reddit.com/")
 
-	for i in re.finditer('[^">](https:\/\/[^ <>]*)', sanitized):
-		sanitized = sanitized.replace(i.group(1), f'<a href="{i.group(1)}">{i.group(1)}</a>')
+	sanitized = re.sub("se(x)", "fuck", sanitized)
+	# for i in re.finditer('[^">](https:\/\/[^ <>]*)', sanitized):
+	# 	sanitized = sanitized.replace(i.group(1), f'<a href="{i.group(1)}">{i.group(1)}</a>')
 
-	for i in re.finditer('<p>(https:\/\/[^ <>]*)', sanitized):
-		sanitized = sanitized.replace(i.group(1), f'<a href="{i.group(1)}">{i.group(1)}</a>')
+	# for i in re.finditer('<p>(https:\/\/[^ <>]*)', sanitized):
+	# 	sanitized = sanitized.replace(i.group(1), f'<a href="{i.group(1)}">{i.group(1)}</a>')
 
 	return sanitized
