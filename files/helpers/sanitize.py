9dd7d65942c5f84c74e319250d8c9cd7ff70f18b
Aevann1 <Aevann3@gmail.com>
2021-09-01 23:11:12 +0200

    fd

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -241,7 +241,7 @@ def sanitize(sanitized, noimages=False):
 	for rd in ["https://reddit.com/", "https://new.reddit.com/", "https://www.reddit.com/", "https://redd.it/"]:
 		sanitized = sanitized.replace(rd, "https://old.reddit.com/")
 
-	sanitized = re.sub("se(x)", "fuck", sanitized)
+	sanitized = re.sub("se(x)", "fuck/1", sanitized)
 	# for i in re.finditer('[^">](https:\/\/[^ <>]*)', sanitized):
 	# 	sanitized = sanitized.replace(i.group(1), f'<a href="{i.group(1)}">{i.group(1)}</a>')
 
