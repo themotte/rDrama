8b727922c9ec2f5177654b1aad9e1dbf1c9e401b
Aevann1 <Aevann3@gmail.com>
2021-09-03 16:51:52 +0200

    fd

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -92,6 +92,7 @@ def settings_profile_post(v):
 		for i in re.finditer('^(https:\/\/.*\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF|9999))', bio, re.MULTILINE): bio = bio.replace(i.group(1), f'![]({i.group(1)})')
 		bio = bio.replace("\n", "\n\n").replace("\n\n\n\n\n\n", "\n\n").replace("\n\n\n\n", "\n\n").replace("\n\n\n", "\n\n")
 
+		print(request.files)
 		# check for uploaded image
 		if request.files.get('file') and request.headers.get("cf-ipcountry") != "T1":
 			
