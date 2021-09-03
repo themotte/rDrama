268d2b482f7dbe26df358176633e5775ed1af1a4
Aevann1 <Aevann3@gmail.com>
2021-09-03 16:54:51 +0200

    fd

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -92,11 +92,9 @@ def settings_profile_post(v):
 		for i in re.finditer('^(https:\/\/.*\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF|9999))', bio, re.MULTILINE): bio = bio.replace(i.group(1), f'![]({i.group(1)})')
 		bio = bio.replace("\n", "\n\n").replace("\n\n\n\n\n\n", "\n\n").replace("\n\n\n\n", "\n\n").replace("\n\n\n", "\n\n")
 
-		print(request.files)
 		# check for uploaded image
 		if request.files.get('file') and request.headers.get("cf-ipcountry") != "T1":
 			
-			print("sex")
 			#check file size
 			if request.content_length > 16 * 1024 * 1024:
 				g.db.rollback()
