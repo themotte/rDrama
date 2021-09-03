8e9426e86b4fb9c7a4aba8f43a88880e828c1ab9
Aevann1 <Aevann3@gmail.com>
2021-09-03 23:36:57 +0200

    fd

--- seed-db.sql
+++ seed-db.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.badge_defs VALUES (6, 'Beta User', 'Joined during open beta', 'beta.png', 3, NULL);
+INSERT INTO public.badge_defs VALUES (6, 'Beta User', 'Joined during open beta', 'beta.gif', 3, NULL);
 
 INSERT INTO public.users (
     id, username, passhash, created_utc, admin_level, over_18, is_activated, bio, bio_html, login_nonce, is_private,
@@ -25,16 +25,6 @@ INSERT INTO public.users (
     unban_utc, original_username, customtitle, defaultsorting, defaulttime, namecolor, titlecolor, profileurl, bannerurl,
     customtitleplain, themecolor, changelogsub, oldreddit, css, profilecss, coins, agendaposter, suicide_utc,
     post_count, comment_count, background, verified 
-) VALUES (2317, 'Sneethe', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'Sneethe', '', 'hot', 'day', '30409f', '30409f', '', '',
-            '', '30409f', false, false, '', '', 0, false, 0, 
-            0, 0, '', true);
-
-INSERT INTO public.users (
-    id, username, passhash, created_utc, admin_level, over_18, is_activated, bio, bio_html, login_nonce, is_private,
-    unban_utc, original_username, customtitle, defaultsorting, defaulttime, namecolor, titlecolor, profileurl, bannerurl,
-    customtitleplain, themecolor, changelogsub, oldreddit, css, profilecss, coins, agendaposter, suicide_utc,
-    post_count, comment_count, background, verified 
 ) VALUES (261, 'Snappy', '', 0, 0, true, true, '', '', 0, false, 
             0, 'Snappy', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
             '', '30409f', false, false, '', '', 0, false, 0, 
@@ -47,6 +37,16 @@ INSERT INTO public.users (
     post_count, comment_count, background, verified 
 ) VALUES (1832, 'longpostbot', '', 0, 0, true, true, '', '', 0, false, 
             0, 'longpostbot', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
+            '', '30409f', false, false, '', '', 0, false, 0, 
+            0, 0, '', true);
+
+INSERT INTO public.users (
+    id, username, passhash, created_utc, admin_level, over_18, is_activated, bio, bio_html, login_nonce, is_private,
+    unban_utc, original_username, customtitle, defaultsorting, defaulttime, namecolor, titlecolor, profileurl, bannerurl,
+    customtitleplain, themecolor, changelogsub, oldreddit, css, profilecss, coins, agendaposter, suicide_utc,
+    post_count, comment_count, background, verified 
+) VALUES (1833, 'zozbot', '', 0, 0, true, true, '', '', 0, false, 
+            0, 'zozbot', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
             '', '30409f', false, false, '', '', 0, false, 0, 
             0, 0, '', true);
 
