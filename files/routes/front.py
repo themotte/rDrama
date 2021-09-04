72600d0e6b19143b11a62fed09c0d91ffcde55a0
Aevann1 <Aevann3@gmail.com>
2021-09-04 03:50:31 +0200

    fdfd

--- files/routes/front.py
+++ files/routes/front.py
@@ -6,33 +6,6 @@ from files.classes.submission import Submission
 
 defaulttimefilter = environ.get("DEFAULT_TIME_FILTER", "day").strip()
 
-class Post(object):
-      
-    def __init__(self, my_dict):
-          
-        for key in my_dict:
-            setattr(self, key, my_dict[key])
-
-@app.get("/marseyverse")
-@auth_desired
-def marseyverse(v):
-	count = 0
-	drama = requests.get("https://rdrama.net/", headers={"Authorization": "sex"}).json()["data"]
-	pcm = requests.get("https://pcmemes.net/", headers={"Authorization": "sex"}).json()["data"]
-	gigachad = requests.get("https://gigachadlife.com/", headers={"Authorization": "sex"}).json()["data"]
-	weebzone = requests.get("https://weebzone.xyz/", headers={"Authorization": "sex"}).json()["data"]
-
-	listing = []
-
-	while count < 100:
-		for site in [drama,pcm,gigachad,weebzone]:
-			try: post = site[count]
-			except: continue
-			listing.append(Post(post))
-		count += 1
-
-	return render_template("marseyverse.html", v=v, listing=listing)
-
 @app.get("/post/")
 def slash_post():
 	return redirect("/")
