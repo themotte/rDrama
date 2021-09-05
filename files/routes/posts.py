628b576270183816b75ff9be992eb5e7fc31bc1b
Aevann1 <Aevann3@gmail.com>
2021-09-05 13:45:22 +0200

    fd

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -181,7 +181,7 @@ def post_id(pid, anything=None, v=None):
 
 		post.preloaded_comments = [x for x in comments if not (x.author and x.author.shadowbanned) or (v and v.id == x.author_id)]
 
-	read = session.get("read_comments")
+	read = session["read_comments"]
 	print(read)
 	read_comments = [x.id for x in post.preloaded_comments]
 	if session.get("read_comments"): session["read_comments"] += read_comments
