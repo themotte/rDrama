a6103ed386e7b14c5687c4aa4a2e0b9bf3117dab
Aevann1 <Aevann3@gmail.com>
2021-09-08 22:56:53 +0200

    fd

--- files/assets/js/general14.js
+++ files/assets/js/general14.js
@@ -419,6 +419,61 @@ function post_toast2(url, button1, button2) {
 }
 
 
+function post_toast3(url, button1, button2) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	var form = new FormData()
+
+	if(typeof data === 'object' && data !== null) {
+		for(let k of Object.keys(data)) {
+				form.append(k, data[k]);
+		}
+	}
+
+
+	form.append("formkey", formkey());
+	xhr.withCredentials=true;
+
+	xhr.onload = function() {
+		if (xhr.status >= 200 && xhr.status < 300) {
+			$('#toast-post-success').toast('dispose');
+			$('#toast-post-success').toast('show');
+			try {
+				document.getElementById('toast-post-success-text').innerText = JSON.parse(xhr.response)["message"];
+			} catch(e) {
+				document.getElementById('toast-post-success-text').innerText = "Action successful!";
+			}
+			try {callback(xhr)}
+			catch(e) {}
+			return true
+
+		} else if (xhr.status >= 300 && xhr.status < 400) {
+			window.location.href = JSON.parse(xhr.response)["redirect"]
+		} else {
+			try {
+				data=JSON.parse(xhr.response);
+
+				$('#toast-post-error').toast('dispose');
+				$('#toast-post-error').toast('show');
+				document.getElementById('toast-post-error-text').innerText = data["error"];
+				return false
+			} catch(e) {
+				$('#toast-post-success').toast('dispose');
+				$('#toast-post-error').toast('dispose');
+				$('#toast-post-error').toast('show');
+				document.getElementById('toast-post-error-text').innerText = "Error. Try again later.";
+				return false
+			}
+		}
+	};
+
+	xhr.send(form);
+
+	document.getElementById(button1).classList.toggle("d-md-inline-block");
+	document.getElementById(button2).classList.toggle("d-md-inline-block");
+}
+
+
 // Search Icon
 // Change navbar search icon when form is in focus, active states
 
