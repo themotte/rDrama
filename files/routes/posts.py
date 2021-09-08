967f01a2ad69d2d0c52774486b1feebdf659732c
Aevann1 <Aevann3@gmail.com>
2021-09-08 22:22:27 +0200

    fd

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -894,53 +894,53 @@ def submit_post(v):
 		if 'pcm' in request.host:
 			if file.content_type.startswith('image/'):
 				new_post.url = upload_ibb(file)
-			# else:
-			# 	try:
-			# 		post_url = upload_video(file)
-			# 		if not post_url.endswith('.mp4'):
-			# 			post_url += 'mp4'
-			# 		new_post.url = post_url
-			# 		new_post.processing = True
-			# 		gevent.spawn(check_processing_thread, v.id, new_post, post_url, g.db)
-			# 	except UploadException as e:
-			# 		if request.headers.get("Authorization"):
-			# 			return {
-			# 				"error": str(e),
-			# 			}, 400
-			# 		else:
-			# 			return render_template(
-			# 				"submit.html",
-			# 				v=v,
-			# 				error=str(e),
-			# 				title=title,
-			# 				body=request.form.get("body", "")
-			# 			), 400
+			else:
+				try:
+					post_url = upload_video(file)
+					if not post_url.endswith('.mp4'):
+						post_url += 'mp4'
+					new_post.url = post_url
+					new_post.processing = True
+					gevent.spawn(check_processing_thread, v.id, new_post, post_url, g.db)
+				except UploadException as e:
+					if request.headers.get("Authorization"):
+						return {
+							"error": str(e),
+						}, 400
+					else:
+						return render_template(
+							"submit.html",
+							v=v,
+							error=str(e),
+							title=title,
+							body=request.form.get("body", "")
+						), 400
 		else:
 			if file.content_type.startswith('image/'):
 				new_post.url = upload_imgur(file)
-			# else:
-			# 	try:
-			# 		post_url = upload_video(file)
-			# 		# shit to make webm work
-			# 		if not post_url.endswith('.mp4'):
-			# 			post_url += 'mp4'
-			# 		# print(post_url)
-			# 		new_post.url = post_url
-			# 		new_post.processing = True
-			# 		gevent.spawn(check_processing_thread, v.id, new_post, post_url, g.db)
-			# 	except UploadException as e:
-			# 		if request.headers.get("Authorization"):
-			# 			return {
-			# 				"error": str(e),
-			# 			}, 400
-			# 		else:
-			# 			return render_template(
-			# 				"submit.html",
-			# 				v=v,
-			# 				error=str(e),
-			# 				title=title,
-			# 				body=request.form.get("body", "")
-			# 			), 400
+			else:
+				try:
+					post_url = upload_video(file)
+					# shit to make webm work
+					if not post_url.endswith('.mp4'):
+						post_url += 'mp4'
+					# print(post_url)
+					new_post.url = post_url
+					new_post.processing = True
+					gevent.spawn(check_processing_thread, v.id, new_post, post_url, g.db)
+				except UploadException as e:
+					if request.headers.get("Authorization"):
+						return {
+							"error": str(e),
+						}, 400
+					else:
+						return render_template(
+							"submit.html",
+							v=v,
+							error=str(e),
+							title=title,
+							body=request.form.get("body", "")
+						), 400
 
 		g.db.add(new_post)
 		g.db.add(new_post.submission_aux)
