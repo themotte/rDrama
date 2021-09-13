e7eea029e8df22cbee20bb44fb3ed1e366b12ad2
Aevann1 <Aevann3@gmail.com>
2021-09-13 16:56:14 +0200

    fd

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -24,17 +24,6 @@ from files.helpers.discord import add_role
 IMGUR_KEY = environ.get("IMGUR_KEY", "").strip()
 SITE_NAME = environ.get("SITE_NAME", "").strip()
 
-@app.get("/sexing")
-def nig():
-	first = time.time()
-	sex = [c for c in g.db.query(User).all() if c.id == 1]
-	print(time.time() - first)
-	first2 = time.time()
-	sex2 = g.db.query(User).all()
-	sex2 = [c for c in sex2 if c.id == 1]
-	print(time.time() - first2)
-	return "fug"
-
 @app.post("/@<username>/revert_actions")
 @admin_level_required(6)
 def revert_actions(v, username):
