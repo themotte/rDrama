c05dd071e2d4052061620d416b701a2b80f6833f
Aevann1 <Aevann3@gmail.com>
2021-09-17 11:12:54 +0200

    fd

--- files/__main__.py
+++ files/__main__.py
@@ -83,7 +83,7 @@ limiter = Limiter(
 _engine=create_engine(
 	app.config['DATABASE_URL'],
 	poolclass=QueuePool,
-	pool_size=int(environ.get("PG_POOL_SIZE",10)),
+	pool_size=197,
 	pool_use_lifo=True
 )
 
