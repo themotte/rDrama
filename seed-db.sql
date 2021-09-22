f004e84bf9a28b437bf7847802da0c589b9071be
atrc445 <atrc445@protonmail.com>
2021-09-22 13:43:21 +0200

    fds

--- seed-db.sql
+++ seed-db.sql
@@ -28,29 +28,29 @@ insert into public.award_relationships(id,user_id,kind) values(1,1,'shit');
 
 INSERT INTO public.users (
     id, username, passhash, created_utc, admin_level, over_18, is_activated, bio, bio_html, login_nonce, is_private,
-    unban_utc, original_username, customtitle, defaultsorting, defaulttime, namecolor, titlecolor, profileurl, bannerurl,
+    unban_utc, original_username, customtitle, defaultsorting, defaultsortingcomments, defaulttime, namecolor, titlecolor, profileurl, bannerurl,
     customtitleplain, theme, themecolor, changelogsub, oldreddit, css, profilecss, coins, agendaposter, suicide_utc,
-    post_count, comment_count, background, verified 
+    post_count, comment_count, background, verified, truecoins
 ) VALUES (1, 'Drama', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'Drama', '', 'hot', 'day', 'ff66ac', 'ff66ac', '', '',
+            0, 'Drama', '', 'hot', 'top', 'day', 'ff66ac', 'ff66ac', '', '',
             '', 'dark', 'ff66ac', false, false, '', '', 0, false, 0, 
-            0, 0, '', true),
+            0, 0, '', true, 0),
          (2, 'AutoJanny', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'AutoJanny', '', 'hot', 'day', 'ff66ac', 'ff66ac', '', '',
+            0, 'AutoJanny', '', 'hot', 'top', 'day', 'ff66ac', 'ff66ac', '', '',
             '', 'dark', 'ff66ac', false, false, '', '', 0, false, 0, 
-            0, 0, '', true),
+            0, 0, '', true, 0),
          (3, 'Snappy', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'Snappy', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
+            0, 'Snappy', '', 'hot', 'top', 'day', '62ca56', 'e4432d', '', '',
             '', 'dark', '30409f', false, false, '', '', 0, false, 0, 
-            0, 0, '', true),
+            0, 0, '', true, 0),
          (4, 'longpostbot', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'longpostbot', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
+            0, 'longpostbot', '', 'hot', 'top', 'day', '62ca56', 'e4432d', '', '',
             '', 'dark', '30409f', false, false, '', '', 0, false, 0, 
-            0, 0, '', true),
+            0, 0, '', true, 0),
          (5, 'zozbot', '', 0, 0, true, true, '', '', 0, false, 
-            0, 'zozbot', '', 'hot', 'day', '62ca56', 'e4432d', '', '',
+            0, 'zozbot', '', 'hot', 'top', 'day', '62ca56', 'e4432d', '', '',
             '', 'dark', '30409f', false, false, '', '', 0, false, 0, 
-            0, 0, '', true);
+            0, 0, '', true, 0);
 
 
 SELECT pg_catalog.setval('public.users_id_seq', 5, true);
