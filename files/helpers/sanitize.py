d97af1e25ea1389d00b5009a239b50517e2406f6
Aevann1 <Aevann3@gmail.com>
2021-09-22 23:54:11 +0200

    dfs

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -220,7 +220,7 @@ def sanitize(sanitized, noimages=False):
 	for i in re.finditer('<p>:([^ ]{1,30}?):</p>', sanitized):
 		emoji = i.group(1).lower()
 		if path.isfile(f'./files/assets/images/emojis/{emoji}.webp'):
-			sanitized = re.sub(f'<p>:{emoji}:</p>', f'<p style="margin-bottom:0;"><img loading="lazy" data-toggle="tooltip" alt=":{emoji}:" title=":{emoji}:" delay="0" height=60 src="https://{site}/assets/images/emojis/{emoji}.webp"></p>')
+			sanitized = re.sub(f'<p>:{emoji}:</p>', f'<p style="margin-bottom:0;"><img loading="lazy" data-toggle="tooltip" alt=":{emoji}:" title=":{emoji}:" delay="0" height=60 src="https://{site}/assets/images/emojis/{emoji}.webp"></p>', sanitized)
 
 			try:
 				if emoji in session["favorite_emojis"]: session["favorite_emojis"][emoji] += 1
