2b73837a469e432d98cb3fffc0bb7d291d34ec06
Aevann1 <Aevann3@gmail.com>
2021-09-24 01:03:08 +0200

    sfd

--- files/__main__.py
+++ files/__main__.py
@@ -120,7 +120,6 @@ def after_request(response):
 
 	response.headers.add("Strict-Transport-Security", "max-age=31536000")
 	response.headers.add("Referrer-Policy", "same-origin")
-	response.headers.add("Feature-Policy", "geolocation 'none'; midi 'none'; notifications 'none'; push 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; vibrate 'none'; fullscreen 'none'; payment 'none';")
 	response.headers.add("X-Frame-Options", "deny")
 	return response
 
