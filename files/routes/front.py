3ebc5ee4602443e82db65a59a126297f92fb0794
Aevann1 <Aevann3@gmail.com>
2021-09-24 05:17:13 +0200

    fds

--- files/routes/front.py
+++ files/routes/front.py
@@ -167,7 +167,7 @@ def frontlist(v=None, sort="hot", page=1, t="all", ids_only=True, filter_words='
 		posts = posts.filter(Submission.created_utc < lt)
 
 	if not (v and v.shadowbanned):
-		posts = posts.join(Submission.author.shadowbanned).filter(User.shadowbanned == False)
+		posts = posts.join(Submission.author.shadowbanned).filter(shadowbanned == False)
 
 	if sort == "hot":
 		ti = int(time.time())
