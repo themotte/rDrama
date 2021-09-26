27217ac2b9949d806c6fd732a9604618aac95948
Aevann1 <Aevann3@gmail.com>
2021-09-26 13:33:12 +0200

    dfsfs

--- files/routes/front.py
+++ files/routes/front.py
@@ -170,7 +170,7 @@ def frontlist(v=None, sort="hot", page=1, t="all", ids_only=True, filter_words='
 			cutoff = now - 2592000
 		elif t == 'year':
 			cutoff = now - 31536000
-		posts = posts.filter(Submission.created_utc >= cutoff)
+		if cutoff: posts = posts.filter(Submission.created_utc >= cutoff)
 
 	posts = posts.filter_by(is_banned=False, stickied=None, private=False, deleted_utc = 0)
 
