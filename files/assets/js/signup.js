9802331bb64fb735485981eb206277a69aa166ab
Aevann1 <Aevann3@gmail.com>
2021-09-27 22:16:32 +0200

    fsd

--- files/assets/js/signup.js
+++ files/assets/js/signup.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById('password-register').addEventListener('input', function () {
+	document.getElementById('password-register').addEventListener('input', function () {
 
-        var charCount = document.getElementById("password-register").value;
-        var id = document.getElementById("passwordHelpRegister");
-        var successID = document.getElementById("passwordHelpSuccess");
+		var charCount = document.getElementById("password-register").value;
+		var id = document.getElementById("passwordHelpRegister");
+		var successID = document.getElementById("passwordHelpSuccess");
 
-        if (charCount.length >= 8) {
-            id.classList.add("d-none");
-            successID.classList.remove("d-none");
-        } else {
-            id.classList.remove("d-none");
-            successID.classList.add("d-none");
-        }
-    });
+		if (charCount.length >= 8) {
+			id.classList.add("d-none");
+			successID.classList.remove("d-none");
+		} else {
+			id.classList.remove("d-none");
+			successID.classList.add("d-none");
+		}
+	});
 
-    document.getElementById('username-register').addEventListener('input', function () {
+	document.getElementById('username-register').addEventListener('input', function () {
 
-        const userName = document.getElementById("username-register").value;
-        const id = document.getElementById("usernameHelpRegister");
+		const userName = document.getElementById("username-register").value;
+		const id = document.getElementById("usernameHelpRegister");
 
-        const API = '/is_available/' + userName;
-        if (userName.length >= 3) {
-                // response is {"<userName>": false} and if false it means it already exists
-            fetch(API)
-                .then(res => res.json())
-                .then(json => {
-                    if (!json[userName]) {
-                        id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username already taken :(</span>';
-                    }
-                })
-        }
+		const API = '/is_available/' + userName;
+		if (userName.length >= 3) {
+				// response is {"<userName>": false} and if false it means it already exists
+			fetch(API)
+				.then(res => res.json())
+				.then(json => {
+					if (!json[userName]) {
+						id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username already taken :(</span>';
+					}
+				})
+		}
 
-        if (!/[^a-zA-Z0-9_\-$]/.test(userName)) {
-            id.innerHTML = '<span class="form-text font-weight-bold text-success mt-1">Username is a-okay!</span>';
+		if (!/[^a-zA-Z0-9_\-$]/.test(userName)) {
+			id.innerHTML = '<span class="form-text font-weight-bold text-success mt-1">Username is a-okay!</span>';
 
-            if (userName.length < 3) {
-                id.innerHTML = '<span class="form-text font-weight-bold text-muted mt-1">Username must be at least 3 characters long.</span>';
-            } else if (userName.length > 25) {
-                id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username must be 25 characters or less.</span>';
-            }
-        } else {
-            id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">No special characters or spaces allowed.</span>';
-        }
+			if (userName.length < 3) {
+				id.innerHTML = '<span class="form-text font-weight-bold text-muted mt-1">Username must be at least 3 characters long.</span>';
+			} else if (userName.length > 25) {
+				id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username must be 25 characters or less.</span>';
+			}
+		} else {
+			id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">No special characters or spaces allowed.</span>';
+		}
 
-    });
+	});
 
 });
