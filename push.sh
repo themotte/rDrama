367e79539358d17bb1d8ba8aa2499eda2133e629
Aevann1 <Aevann3@gmail.com>
2021-09-29 03:47:46 +0200

    gd

--- push.sh
+++ push.sh
@@ -5,14 +5,14 @@ git push
 
 npm install -g sass
 apt install ruby-sass
-sass ./files/assets/css/transparent.scss ./files/assets/css/transparent_ff66ac.css
-sass ./files/assets/css/win98.scss ./files/assets/css/win98_ff66ac.css
-sass ./files/assets/css/midnight.scss ./files/assets/css/midnight_ff66ac.css
-sass ./files/assets/css/dark.scss ./files/assets/css/dark_ff66ac.css
-sass ./files/assets/css/light.scss ./files/assets/css/light_ff66ac.css
-sass ./files/assets/css/coffee.scss ./files/assets/css/coffee_ff66ac.css
-sass ./files/assets/css/tron.scss ./files/assets/css/tron_ff66ac.css
-sass ./files/assets/css/4chan.scss ./files/assets/css/4chan_ff66ac.css
+sass ./files/assets/css2/transparent.scss ./files/assets/css2/transparent_ff66ac.css
+sass ./files/assets/css2/win98.scss ./files/assets/css2/win98_ff66ac.css
+sass ./files/assets/css2/midnight.scss ./files/assets/css2/midnight_ff66ac.css
+sass ./files/assets/css2/dark.scss ./files/assets/css2/dark_ff66ac.css
+sass ./files/assets/css2/light.scss ./files/assets/css2/light_ff66ac.css
+sass ./files/assets/css2/coffee.scss ./files/assets/css2/coffee_ff66ac.css
+sass ./files/assets/css2/tron.scss ./files/assets/css2/tron_ff66ac.css
+sass ./files/assets/css2/4chan.scss ./files/assets/css2/4chan_ff66ac.css
 python ./compilecss.py
 python3 ./compilecss.py
 git add .
