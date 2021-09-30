50d5ffc9184710e5fae642f3e2ad8ca86bc048be
Aevann1 <Aevann3@gmail.com>
2021-09-30 23:01:52 +0200

    sfdsfd

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -217,16 +217,18 @@ def sanitize(sanitized, noimages=False):
 
 	if start in sanitized and end in sanitized and start in sanitized.split(end)[0] and end in sanitized.split(start)[1]: 			sanitized = sanitized.replace(start, '<span class="spoiler">').replace(end, '</span>')
 	
-	for i in re.finditer('<p>:([^ ]{1,30}?):</p>', sanitized):
-		emoji = i.group(1).lower()
-		if path.isfile(f'./files/assets/images/emojis/{emoji}.webp'):
-			sanitized = sanitized.replace(f'<p>:{emoji}:</p>', f'<p style="margin-bottom:0;"><img loading="lazy" data-bs-toggle="tooltip" alt=":{emoji}:" title=":{emoji}:" delay="0" height=60 src="https://{site}/assets/images/emojis/{emoji}.webp"></p>')
+	for i in re.finditer('^(\s+)?((:[\w]+:)+(\s+)?)+$', sanitized):
+		emojis = i.group(0).lower()
+		for i in re.finditer('\w*(?<!"):([^ ]{1,30}?):', emojis):
+			emoji = i.group(1)
+			if path.isfile(f'./files/assets/images/emojis/{emoji}.webp'):
+				sanitized = sanitized.replace(f'<p>:{emoji}:</p>', f'<p style="margin-bottom:0;"><img loading="lazy" data-bs-toggle="tooltip" alt=":{emoji}:" title=":{emoji}:" delay="0" height=60 src="https://{site}/assets/images/emojis/{emoji}.webp"></p>')
 
-			try:
-				if emoji in session["favorite_emojis"]: session["favorite_emojis"][emoji] += 1
-				else: session["favorite_emojis"][emoji] = 1
-			except:
-				pass
+				try:
+					if emoji in session["favorite_emojis"]: session["favorite_emojis"][emoji] += 1
+					else: session["favorite_emojis"][emoji] = 1
+				except:
+					pass
 
 	for i in re.finditer('\w*(?<!"):([^ ]{1,30}?):', sanitized):
 		emoji = i.group(1).lower()
