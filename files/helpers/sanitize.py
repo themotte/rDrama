762e062427b1e77874b5768141bc0ae03c12330d
Aevann1 <Aevann3@gmail.com>
2021-09-30 23:14:17 +0200

    dfs

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -217,8 +217,9 @@ def sanitize(sanitized, noimages=False):
 
 	if start in sanitized and end in sanitized and start in sanitized.split(end)[0] and end in sanitized.split(start)[1]: 			sanitized = sanitized.replace(start, '<span class="spoiler">').replace(end, '</span>')
 	
-	for i in re.findall(r"^(\s+)?((:[\w]+:)+(\s+)?)+", sanitized):
-		emojis = i.lower()
+	for i in re.finditer(r"^(\s+)?((:[\w]+:)+(\s+)?)+", sanitized):
+		print(i)
+		emojis = i.group(0).lower()
 		print(emojis)
 		for i in re.finditer('\w*(?<!"):([^ ]{1,30}?):', emojis):
 			emoji = i.group(1)
