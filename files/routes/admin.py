cfa753727cd366e81723923ef188ab990001de58
Aevann1 <Aevann3@gmail.com>
2021-09-30 22:56:25 +0200

    fds

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -249,7 +249,7 @@ def post_rules(v):
 @auth_required
 def shadowbanned(v):
 	if not (v and v.admin_level == 6): abort(404)
-	users = [x for x in g.db.query(User).options(lazyload('*')).filter_by(shadowbanned = True).all()]
+	users = [x for x in g.db.query(User).options(lazyload('*')).filter_by(shadowbanned != None).all()]
 	return render_template("banned.html", v=v, users=users)
 
 
