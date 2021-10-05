672afac235d9e734b72c595b55dd446349947a78
Aevann1 <Aevann3@gmail.com>
2021-10-06 01:21:23 +0200

    dfsfds

--- files/assets/js/comments_v.js
+++ files/assets/js/comments_v.js
@@ -7,7 +7,7 @@ function post(url, callback, errortext) {
     xhr.onerror=function() { alert(errortext); };
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
-            callback();
+            if (typeof callback === "function") callback();
         } else {
             xhr.onerror();
         }
