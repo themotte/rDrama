9f8adfd23545e30883f98c1a00276b06386a33ce
Aevann1 <Aevann3@gmail.com>
2021-10-05 21:58:04 +0200

    dsfsfd

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -212,11 +212,9 @@ def sanitize(sanitized, noimages=False):
 
 	for i in re.finditer("<p>\s*((!\w+!)\s*)+<\/p>", sanitized):
 		old = i.group(0)
-		print(old)
 		new = old.lower().replace("<p>", "<p style='margin-bottom:0'>")
 		for i in re.finditer('\w*(?<!")!([^ ]{1,30}?)!', new):
 			emoji = i.group(1).lower()
-			print(emoji)
 			if path.isfile(f'./files/assets/images/emojis/{emoji}.webp'):
 				new = re.sub(f'\w*(?<!")!{emoji}!', f'<img loading="lazy" data-bs-toggle="tooltip" alt="!{emoji}!" title="!{emoji}!" delay="0" height=60 src="https://{site}/assets/images/emojis/{emoji}.webp" style="transform: scaleX(-1)">', new)
 				
