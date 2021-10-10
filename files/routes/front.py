f0692dbe8d450790a8ce0e33ded8412ed20ad763
Aevann1 <Aevann3@gmail.com>
2021-10-10 16:19:07 +0200

    dfsfd

--- files/routes/front.py
+++ files/routes/front.py
@@ -199,7 +199,7 @@ def frontlist(v=None, sort="hot", page=1, t="all", ids_only=True, filter_words='
 	elif sort == "old":
 		posts = posts.order_by(Submission.created_utc.asc())
 	elif sort == "controversial":
-		posts = posts.order_by(-1 * Submission.upvotes * min(Submission.downvotes, 1))
+		posts = posts.order_by(-1 * Submission.upvotes * func.min(Submission.downvotes, 1))
 	elif sort == "top":
 		posts = posts.order_by(Submission.downvotes - Submission.upvotes)
 	elif sort == "bottom":
@@ -291,7 +291,7 @@ def changeloglist(v=None, sort="new", page=1 ,t="all"):
 	elif sort == "old":
 		posts = posts.order_by(Submission.created_utc.asc())
 	elif sort == "controversial":
-		posts = posts.order_by(-1 * Submission.upvotes * min(Submission.downvotes, 1))
+		posts = posts.order_by(-1 * Submission.upvotes * func.min(Submission.downvotes, 1))
 	elif sort == "top":
 		posts = posts.order_by(Submission.downvotes - Submission.upvotes)
 	elif sort == "bottom":
@@ -361,7 +361,7 @@ def comment_idlist(page=1, v=None, nsfw=False, sort="new", t="all"):
 	elif sort == "old":
 		comments = comments.order_by(Comment.created_utc.asc())
 	elif sort == "controversial":
-		comments = comments.order_by(-1 * Comment.upvotes * min(Comment.downvotes, 1))
+		comments = comments.order_by(-1 * Comment.upvotes * func.min(Comment.downvotes, 1))
 	elif sort == "top":
 		comments = comments.order_by(Comment.downvotes - Comment.upvotes)
 	elif sort == "bottom":
