7acaa16988c9c733dae65175ee5e354e4e10cf4e
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2021-10-25 01:47:04 +0200

    Delete INSERT INTO public.badge_defs VALUES (67.sql

--- INSERT INTO public.badge_defs VALUES (67.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.badge_defs VALUES (67, 'Unpausable', 'Spent 40,000 dramacoins on an unpausable profile anthem', 'unpausable', 3, NULL);
-INSERT INTO public.badge_defs VALUES (68, 'Pause Button', 'Spent 20,000 dramacoins on a profile anthem pause button', 'pause', 3, NULL);
-INSERT INTO public.badge_defs VALUES (69, 'Little Big Spender', 'Dropped 10,000 dramacoins at the shop', 'coins_spent1', 3, NULL);
-INSERT INTO public.badge_defs VALUES (70, 'Big Spender', 'Dropped 100,000 dramacoins at the shop', 'coins_spent2', 3, NULL);
-INSERT INTO public.badge_defs VALUES (71, 'Big Big Spender', 'Dropped 250,000 dramacoins at the shop', 'coins_spent3', 3, NULL);
-INSERT INTO public.badge_defs VALUES (72, 'BIG Big Spender', 'Dropped 500,000 dramacoins at the shop', 'coins_spent4', 3, NULL);
-INSERT INTO public.badge_defs VALUES (73, 'Le Rich Gentlesir', 'Spent a fucking million dramacoins at the shop', 'coins_spent5', 3, NULL);
