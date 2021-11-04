73d103db0511340fa9008e246b3f4989d4d61304
Aevann1 <randomname42029@gmail.com>
2021-11-04 20:41:29 +0200

    vdfsfd

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -30,7 +30,7 @@ def toggle_club(pid, v):
 
 	post = get_post(pid)
 
-	if post.author_id != v.id or not v.paid_dues: abort(403)
+	if post.author_id != v.id or v.admin_level == 0: abort(403)
 
 	post.club = not post.club
 	g.db.add(post)
