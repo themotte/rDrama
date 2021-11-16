4943f304d9e0c0fbf912d68f33d3b85866217189
Aevann1 <randomname42029@gmail.com>
2021-11-16 23:37:09 +0200

    fsdfds

--- files/classes/mod_logs.py
+++ files/classes/mod_logs.py
@@ -82,7 +82,7 @@ class ModAction(Base):
 	@lazy
 	def string(self):
 
-		output =  ACTIONTYPES[self.kind]["str"].format(self=self).format(cc=cc)
+		output =  ACTIONTYPES[self.kind]["str"].format(self=self, cc=cc)
 
 		if self.note: output += f" <i>({self.note})</i>"
 
