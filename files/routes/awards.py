c8fc8d3ae3293783a58a7522c62c84455925ff61
Aevann1 <randomname42029@gmail.com>
2021-11-24 00:48:16 +0200

    fdsfds

--- files/routes/awards.py
+++ files/routes/awards.py
@@ -561,6 +561,7 @@ def award_post(pid, v):
 		else: author.longpost = time.time() + 86400
 		send_notification(IDIO_ID, f"@{v.username} used {kind} award on {request.host_url}{post.shortlink}")
 	elif kind == "bird":
+		if author.longpost: return {"error": "This user is the under the effect of a conflicting award: Pizzashill award."}, 404
 		if author.bird: author.bird += 86400
 		else: author.bird = time.time() + 86400
 	elif kind == "eye":
@@ -704,6 +705,8 @@ def award_comment(cid, v):
 		else: author.longpost = time.time() + 86400
 		send_notification(IDIO_ID, f"@{v.username} used {kind} award on {request.host_url}{c.shortlink}")
 	elif kind == "bird":
+		if author.longpost:
+			return {"error": "This user is the under the effect of a conflicting award: Pizzashill award."}, 404
 		if author.bird: author.bird += 86400
 		else: author.bird = time.time() + 86400
 	elif kind == "eye":
