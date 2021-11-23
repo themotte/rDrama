3e2302ba2a84011ce9ba9881b714d03b6f4d9894
Aevann1 <randomname42029@gmail.com>
2021-11-24 00:44:16 +0200

    sfdfds

--- files/helpers/alerts.py
+++ files/helpers/alerts.py
@@ -14,7 +14,10 @@ def send_notification(uid, text, autojanny=False):
 	text_html = sanitize(text_html)
 	
 	if autojanny: author_id = AUTOJANNY_ID
-	else: author_id = NOTIFICATIONS_ID
+	else:
+		author_id = NOTIFICATIONS_ID
+		existing = g.db.query(Comment.id).filter(Comment.author_id == author_id, Comment.body_html == text_html, Comment.notifiedto == uid).first()
+		if existing: return
 
 	new_comment = Comment(author_id=author_id,
 							parent_submission=None,
