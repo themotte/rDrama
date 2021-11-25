fc15bd6c2786373f44ba25cbf8b700e0d1126f34
fireworks88 <throwaway.546372819@protonmail.com>
2021-11-25 23:01:09 +0100

    sex

--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,12 @@ module.exports = (ctx) => ({
   plugins: [
     require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        path.resolve(__dirname, 'templates/**/*.html'),
-        path.resolve(__dirname, 'templates/*.html')
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
+    //require('@fullhuman/postcss-purgecss')({
+    //  content: [
+    //    path.resolve(__dirname, 'templates/**/*.html'),
+    //    path.resolve(__dirname, 'templates/*.html')
+    //  ],
+    //  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    //})
   ],
 });
