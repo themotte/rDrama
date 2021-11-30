571beef7a563374a726dedd452fadee1c8c1f81d
Aevann1 <randomname42029@gmail.com>
2021-12-01 01:21:29 +0200

    dfdf

--- files/classes/user.py
+++ files/classes/user.py
@@ -251,11 +251,11 @@ class User(Base):
 		elif sort == "old":
 			posts = posts.order_by(Submission.created_utc.asc())
 		elif sort == "controversial":
-			posts = posts.order_by(-1 * Submission.realupvotes * Submission.downvotes * Submission.downvotes)
+			posts = posts.order_by(-1 * Submission.upvotes * Submission.downvotes * Submission.downvotes)
 		elif sort == "top":
-			posts = posts.order_by(Submission.downvotes - Submission.realupvotes)
+			posts = posts.order_by(Submission.downvotes - Submission.upvotes)
 		elif sort == "bottom":
-			posts = posts.order_by(Submission.realupvotes - Submission.downvotes)
+			posts = posts.order_by(Submission.upvotes - Submission.downvotes)
 		elif sort == "comments":
 			posts = posts.order_by(Submission.comment_count.desc())
 
