9378fa96e3d5178dab92bb399fbd8a47cde24fd4
Aevann1 <randomname42029@gmail.com>
2021-11-30 20:47:03 +0200

    fsdfsd

--- files/assets/js/popover.js
+++ files/assets/js/popover.js
@@ -1,34 +1,36 @@
-function eventasdf(value){
-    var content_id = value.getAttributeNode("data-content-id").value;
-    value.addEventListener("click", function(){jhkj(content_id)});
-  }
-  
-  function checkIfBussy(){
-    if (document.getElementById("bussy") != null){
-      document.body.removeChild(document.getElementById("bussy"));
+window.addEventListener('DOMContentLoaded', function() {
+  function eventasdf(value){
+      var content_id = value.getAttributeNode("data-content-id").value;
+      value.addEventListener("click", function(){jhkj(content_id)});
     }
-  }
-  
-  function dfgh(e){
-    active = document.activeElement;
-    if (active.getAttributeNode("class") == null || active.getAttributeNode("class").nodeValue != "user-name text-decoration-none"){
+    
+    function checkIfBussy(){
+      if (document.getElementById("bussy") != null){
+        document.body.removeChild(document.getElementById("bussy"));
+      }
+    }
+    
+    function dfgh(e){
+      active = document.activeElement;
+      if (active.getAttributeNode("class") == null || active.getAttributeNode("class").nodeValue != "user-name text-decoration-none"){
+        checkIfBussy();
+      }
+    }
+    
+    function jhkj(value){
       checkIfBussy();
+      var popover_shit = document.getElementsByClassName("popover")[0];
+      var uiop = document.createElement("DIV");
+    
+      uiop.innerHTML = popover_shit.outerHTML;
+      uiop.id = "bussy";
+    
+      document.body.appendChild(uiop);
+      document.body.removeChild(popover_shit);
     }
-  }
-  
-  function jhkj(value){
-    checkIfBussy();
-    var popover_shit = document.getElementsByClassName("popover")[0];
-    var uiop = document.createElement("DIV");
-  
-    uiop.innerHTML = popover_shit.outerHTML;
-    uiop.id = "bussy";
-  
-    document.body.appendChild(uiop);
-    document.body.removeChild(popover_shit);
-  }
-  
-  var usernames = document.querySelectorAll("a.user-name");
-  usernames.forEach(eventasdf);
-  
-  document.addEventListener("click", function(e){dfgh(e)});  
+    
+    var usernames = document.querySelectorAll("a.user-name");
+    usernames.forEach(eventasdf);
+    
+    document.addEventListener("click", function(e){dfgh(e)});
+});
