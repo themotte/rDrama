878bf71abe6bbcb1bc313d92f886115772dbbfdd
kek7198 <grayboy99@protonmail.com>
2021-12-03 21:54:18 -0600

    fix

--- files/assets/js/submission_listing.js
+++ files/assets/js/submission_listing.js
@@ -4,7 +4,7 @@ function timestamp(str, ti) {
 };
 
 function expandText(id) {
-    document.getElementById('post-text-'+id).classList.toggle('d-none');
+    document.getElementById('post-text-'+id).classList.toggle('hidden');
     document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-expand-alt');
     document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-compress-alt');
 };
