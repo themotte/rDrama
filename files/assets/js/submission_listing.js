4452dad1b1766cb3e05ae52706a964362de6d5e4
kek7198 <grayboy99@protonmail.com>
2021-12-03 21:50:01 -0600

    fix

--- files/assets/js/submission_listing.js
+++ files/assets/js/submission_listing.js
@@ -8,22 +8,3 @@ function expandText(id) {
     document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-expand-alt');
     document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-compress-alt');
 };
-
-window.addEventListener("load",function(event) {
-    var clipboard = new ClipboardJS('.copy-link');
-    clipboard.on('success', function(e) {
-        var myToast = new bootstrap.Toast(document.getElementById('toast-success'));
-        myToast.show();
-    });
-
-    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-
-    const popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-        const popoverId = popoverTriggerEl.getAttribute('data-content-id');
-        const contentEl = document.getElementById(popoverId).innerHTML;
-        return new bootstrap.Popover(popoverTriggerEl, {
-            content: contentEl,
-            html: true,
-        });
-    })
-});
