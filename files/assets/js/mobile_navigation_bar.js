6c2340fe010067e826ba677a3c7d79979fedf140
kek7198 <grayboy99@protonmail.com>
2021-12-06 17:50:00 -0600

    fix

--- files/assets/js/mobile_navigation_bar.js
+++ files/assets/js/mobile_navigation_bar.js
@@ -1,44 +1,42 @@
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
+
 window.onscroll = function () {
-	var currentScrollPos = window.pageYOffset;
 
-	var topBar = document.getElementById("fixed-bar-mobile");
-
+	let currentScrollPos = window.pageYOffset;
+	// var topBar = document.getElementById("fixed-bar-mobile");
 	var bottomBar = document.getElementById("mobile-bottom-navigation-bar");
-
-	var dropdown = document.getElementById("mobileSortDropdown");
-
-	var navbar = document.getElementById("navbar");
+	// var dropdown = document.getElementById("mobileSortDropdown");
+	// var navbar = document.getElementById("navbar");
 
 	if (bottomBar != null) {
 		if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
-			bottomBar.style.bottom = "0px";
+			bottomBar.style.transform = "translateY(0px)"
 		} 
 		else if (currentScrollPos <= 125 && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
-			bottomBar.style.bottom = "0px";
+			bottomBar.style.transform = "translateY(0px)";
 		}
 		else if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) >= (document.body.offsetHeight - 65)) {
-			bottomBar.style.bottom = "-50px";
+			bottomBar.style.transform = "translateY(-50px)"
 		}
 		else {
-			bottomBar.style.bottom = "-50px";
+			bottomBar.style.transform = "translateY(-50px)"
 		}
 	}
 
-	if (topBar != null && dropdown != null) {
-		if (prevScrollpos > currentScrollPos) {
-			topBar.style.top = "48px";
-			navbar.classList.remove("shadow");
-		} 
-		else if (currentScrollPos <= 125) {
-			topBar.style.top = "48px";
-			navbar.classList.remove("shadow");
-		}
-		else {
-			topBar.style.top = "-48px";
-			dropdown.classList.remove('show');
-			navbar.classList.add("shadow");
-		}
-	}
+	// if (topBar != null && dropdown != null) {
+	// 	if (prevScrollpos > currentScrollPos) {
+	// 		topBar.style.top = "48px";
+	// 		navbar.classList.remove("shadow");
+	// 	} 
+	// 	else if (currentScrollPos <= 125) {
+	// 		topBar.style.top = "48px";
+	// 		navbar.classList.remove("shadow");
+	// 	}
+	// 	else {
+	// 		topBar.style.top = "-48px";
+	// 		dropdown.classList.remove('show');
+	// 		navbar.classList.add("shadow");
+	// 	}
+	// }
 	prevScrollpos = currentScrollPos;
 }
