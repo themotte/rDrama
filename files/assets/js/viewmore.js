4c196d229bd2a6151e7405cd95ed6c813d538015
kek7198 <grayboy99@protonmail.com>
2021-12-05 20:07:04 -0600

    fix

--- files/assets/js/viewmore.js
+++ files/assets/js/viewmore.js
@@ -1,23 +1,24 @@
 import { initializeBootstrap } from '/assets/js/modules/initializeBootstrap.js';
 
 function loadMore(pid,sort,offset,id,trigger) {
-    const btn = document.getElementById(trigger) // trigger button
-    const el = document.getElementById(id) // target element to populate
-    const form = new FormData();
-    const xhr = new XMLHttpRequest();
+    // const btn = document.getElementById(trigger) // trigger button
+    // const el = document.getElementById(id) // target element to populate
+    // const form = new FormData();
+    // const xhr = new XMLHttpRequest();
 
-    btn.classList.toggle('animate-pulse');
+    // btn.classList.toggle('animate-pulse');
 
-    xhr.open("post", `/viewmore/${pid}/${sort}/${offset}`);
-    xhr.withCredentials=true;
-    xhr.onload=function(){
-        if (xhr.status==200) {
-            el.innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, ''); // replace desired element with response html
-            btn.style.display = "none"; // hide button
-            initializeBootstrap()
-        } else {
-            btn.disabled = false; // enable our button if GET fails
-        }
-    }
-    xhr.send(form)
+    // xhr.open("post", `/viewmore/${pid}/${sort}/${offset}`);
+    // xhr.withCredentials=true;
+    // xhr.onload=function(){
+    //     if (xhr.status==200) {
+    //         el.innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, ''); // replace desired element with response html
+    //         btn.style.display = "none"; // hide button
+    //         initializeBootstrap()
+    //     } else {
+    //         btn.disabled = false; // enable our button if GET fails
+    //     }
+    // }
+    // xhr.send(form)
+    console.log(pid,sort,offset,id,trigger)
 }
