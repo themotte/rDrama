4be1f236c48351fb6c1acbab4af6b5dc153e11d3
kek7198 <grayboy99@protonmail.com>
2021-12-05 20:15:41 -0600

    fix

--- files/assets/js/header.js
+++ files/assets/js/header.js
@@ -1,8 +1,23 @@
+function initializeBootstrap() {
+    // tooltips
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function(element){
+        return new bootstrap.Tooltip(element);
+    });
+    // popovers
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    let popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        let popoverId = popoverTriggerEl.getAttribute('data-content-id');
+        let contentEl = document.getElementById(popoverId).innerHTML;
+        return new bootstrap.Popover(popoverTriggerEl, {
+            content: contentEl,
+            html: true,
+        });
+    })
+}
+
 document.addEventListener("DOMContentLoaded", function(){
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-	tooltipTriggerList.map(function(element){
-		return new bootstrap.Tooltip(element);
-	});
+	initializeBootstrap()
 });
 
 function post_toast(url, reload, data) {
--- files/assets/js/viewmore.js
+++ files/assets/js/viewmore.js
@@ -1,22 +1,21 @@
-function loadMore() {
-    // const btn = document.getElementById(trigger) // trigger button
-    // const el = document.getElementById(id) // target element to populate
-    // const form = new FormData();
-    // const xhr = new XMLHttpRequest();
+function loadMore(pid,sort,offset,id,trigger) {
+    const btn = document.getElementById(trigger) // trigger button
+    const el = document.getElementById(id) // target element to populate
+    const form = new FormData();
+    const xhr = new XMLHttpRequest();
 
-    // btn.classList.toggle('animate-pulse');
+    btn.classList.toggle('animate-pulse');
 
-    // xhr.open("post", `/viewmore/${pid}/${sort}/${offset}`);
-    // xhr.withCredentials=true;
-    // xhr.onload=function(){
-    //     if (xhr.status==200) {
-    //         el.innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, ''); // replace desired element with response html
-    //         btn.style.display = "none"; // hide button
-    //         initializeBootstrap()
-    //     } else {
-    //         btn.disabled = false; // enable our button if GET fails
-    //     }
-    // }
-    // xhr.send(form)
-    console.log(pid,sort,offset,id,trigger)
+    xhr.open("post", `/viewmore/${pid}/${sort}/${offset}`);
+    xhr.withCredentials=true;
+    xhr.onload=function(){
+        if (xhr.status==200) {
+            el.innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, ''); // replace desired element with response html
+            btn.style.display = "none"; // hide button
+            initializeBootstrap()
+        } else {
+            btn.disabled = false; // enable our button if GET fails
+        }
+    }
+    xhr.send(form)
 }
