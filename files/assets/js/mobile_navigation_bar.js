89aaa8ddbbaaec3d355dcabab00d2276aa849dbd
kek7198 <grayboy99@protonmail.com>
2021-12-06 19:04:02 -0600

    fix

--- files/assets/js/mobile_navigation_bar.js
+++ files/assets/js/mobile_navigation_bar.js
@@ -1,19 +1,46 @@
-let lastKnownScrollPosition = 0;
-let ticking = false;
+let prevScrollpos = window.pageYOffset;
 
-function doSomething(scrollPos) {
-  console.log('test')
+window.onscroll = function () {
+
+	console.log('this works')
+
+	let currentScrollPos = window.pageYOffset;
+	// var topBar = document.getElementById("fixed-bar-mobile");
+	const bottomBar = document.getElementById("mobile-bottom-navigation-bar");
+	// var dropdown = document.getElementById("mobileSortDropdown");
+	// var navbar = document.getElementById("navbar");
+
+	if (bottomBar != null) {
+		if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
+			console.log('this translate y 0 works')
+			bottomBar.style.transform = "translateY(0px)"
+		} 
+		else if (currentScrollPos <= 125 && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
+			bottomBar.style.transform = "translateY(0px)";
+		}
+		else if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) >= (document.body.offsetHeight - 65)) {
+			console.log('this translate y works')
+			bottomBar.style.transform = "translateY(60px)"
+		}
+		else {
+			bottomBar.style.transform = "translateY(60px)"
+		}
+	}
+
+	// if (topBar != null && dropdown != null) {
+	// 	if (prevScrollpos > currentScrollPos) {
+	// 		topBar.style.top = "48px";
+	// 		navbar.classList.remove("shadow");
+	// 	} 
+	// 	else if (currentScrollPos <= 125) {
+	// 		topBar.style.top = "48px";
+	// 		navbar.classList.remove("shadow");
+	// 	}
+	// 	else {
+	// 		topBar.style.top = "-48px";
+	// 		dropdown.classList.remove('show');
+	// 		navbar.classList.add("shadow");
+	// 	}
+	// }
+	prevScrollpos = currentScrollPos;
 }
-
-document.addEventListener('scroll', function(e) {
-  lastKnownScrollPosition = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-});
