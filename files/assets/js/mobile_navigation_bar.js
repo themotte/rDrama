063093634a52135472c44d48baa672ab3c46867f
kek7198 <grayboy99@protonmail.com>
2021-12-06 22:00:15 -0600

    fix

--- files/assets/js/mobile_navigation_bar.js
+++ files/assets/js/mobile_navigation_bar.js
@@ -12,12 +12,12 @@ document.getElementsByTagName('body')[0].onscroll = () => {
 			bottomBar.style.transform = "translateY(60px)";
 			console.log('test 2')
 		}
-		else if (prevScrollpos < currentScrollPos || (currentScrollPos) >= (document.body.offsetHeight)) {
+		else if (prevScrollpos < currentScrollPos && (window.innerHeight + currentScrollPos) >= (document.body.offsetHeight)) {
 			bottomBar.style.transform = "translateY(60px)";
 			console.log(document.body.offsetHeight)
 			console.log((window.innerHeight + currentScrollPos))
 		}
-		else if (prevScrollpos > currentScrollPos && (currentScrollPos) < (document.body.offsetHeight)) {
+		else if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight)) {
 			bottomBar.style.transform = "translateY(0px)"
 			console.log('test 1')
 		}
