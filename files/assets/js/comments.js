9966f6d10ad9485fb843fa56f2140fb518cbd26f
kek7198 <grayboy99@protonmail.com>
2021-12-09 16:09:39 -0600

    fix

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -9,6 +9,14 @@ function collapse_comment(comment_id) {
     const closed = element.classList.toggle("collapsed")
     const top = element.getBoundingClientRect().y
 
+    const text = document.getElementById('comment-text-'+comment_id)
+    const actions = document.getElementById('comment-actions-'+comment_id)
+    const repliesOf = document.getElementById('replies-of-'+comment_id)
+
+    const arr = [text, actions, repliesOf];
+
+    arr.map(x => x.classList.toggle('hidden'));
+
     if (closed && top < 0) {
         element.scrollIntoView()
         window.scrollBy(0, - 100)
