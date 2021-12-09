5acbae54e626287af2c4bf672f0481e304cabc3f
kek7198 <grayboy99@protonmail.com>
2021-12-09 15:06:17 -0600

    fix

--- files/assets/js/popover.js
+++ files/assets/js/popover.js
@@ -29,7 +29,7 @@
     }
     
     
-    var usernames = document.querySelectorAll("butotn.user-name");
+    var usernames = document.querySelectorAll("button.user-name");
     usernames.forEach(eventasdf);
     
     document.addEventListener("click", function(e){dfgh(e)});
