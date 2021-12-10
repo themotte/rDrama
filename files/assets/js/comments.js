d31d4d1425bf6092fbebe8909904f08c2eb0d488
kek7198 <grayboy99@protonmail.com>
2021-12-09 18:47:14 -0600

    fix

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -8,6 +8,10 @@ function collapse_comment(comment_id) {
     const isClosed = comment.classList.contains("collapsed");
     const top = comment.getBoundingClientRect().y;
 
+    const toggler = document.getElementById(`comment-collase-${comment_id}`);
+
+    ['hidden', 'pointer-events-none'].map(x=> comment.classList.toggle(x));
+
     ['items-center', 'opacity-50', 'hover:opacity-100', 'collapsed'].map(y=> comment.classList.toggle(y)); // apply flex alignment and opacity to comment parent wrapping div
 
     if (isClosed && top < 0) {
