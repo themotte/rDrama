78a0a47714db9f884c36ccbb1b62c177abb73ff8
kek7198 <grayboy99@protonmail.com>
2021-12-09 18:32:29 -0600

    fix

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -8,7 +8,7 @@ function collapse_comment(comment_id) {
     const isClosed = comment.classList.contains("collapsed");
     const top = comment.getBoundingClientRect().y;
 
-    ['items-center', 'opacity-50', 'collapsed'].map(y=> comment.classList.toggle(y)); // apply flex alignment and opacity to comment parent wrapping div
+    ['items-center', 'opacity-50', 'hover:opacity-100', 'collapsed'].map(y=> comment.classList.toggle(y)); // apply flex alignment and opacity to comment parent wrapping div
 
     if (isClosed && top < 0) {
         comment.scrollIntoView()
