7d0e0e56e5e72e9b405664847e779e322765e3c4
kek7198 <grayboy99@protonmail.com>
2021-12-09 20:17:01 -0600

    fix

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -11,7 +11,7 @@ function collapse_comment(comment_id) {
 
     ['hidden', 'pointer-events-none'].map(x=> toggler.classList.toggle(x));
 
-    ['items-center', 'opacity-50', 'hover:opacity-100', 'collapsed'].map(y=> comment.classList.toggle(y)); // apply flex alignment and opacity to comment parent wrapping div
+    ['items-center', 'opacity-50', 'md:hover:opacity-100', 'collapsed'].map(y=> comment.classList.toggle(y)); // apply flex alignment and opacity to comment parent wrapping div
 
     const isClosed = comment.classList.contains("collapsed");
 
