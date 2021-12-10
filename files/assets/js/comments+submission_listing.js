da1d8aa9ee5f53d1680e5940af8a2dfe96586dba
Aevann1 <randomname42029@gmail.com>
2021-12-11 00:51:26 +0200

    fsdfsd

--- files/assets/js/comments+submission_listing.js
+++ files/assets/js/comments+submission_listing.js
@@ -32,53 +32,41 @@ function popovertrigger() {
 
 popovertrigger()
 
-function popoverEventListener(value){
-    //get the id of the popover that will be created for this user
-    var content_id = value.getAttributeNode("data-content-id").value;
-    //add an event listener that will trigger the creation of a replacement popover when the username is clicked
-    value.addEventListener("click", function(){createNewPopover(content_id)});
-  }
-  
-  function checkIfPopover(){
-    //check if a replacement popover already exists and remove it if so
-    if (document.getElementById("popover-fix") != null){
-      document.body.removeChild(document.getElementById("popover-fix"));
-    }
-  }
-  
-  function closePopover(e){
-    //get the element that was clicked on
-    active = document.activeElement;
-    //if the element is not a username then check if a popover replacement already exists
-    if (active.attributes.getNamedItem("data-bs-toggle").nodeValue != "popover"){
-      checkIfPopover();
-    }
-  }
-  
-  function createNewPopover(value){
-    //check for an existing replacement popover and then copy the original popover node
-    checkIfPopover();
-    var popover_old = document.getElementsByClassName("popover")[0];
-    var popover_new = document.createElement("DIV");
-  
-    //copy the contents of the original popover
-    popover_new.innerHTML = popover_old.outerHTML;
-    popover_new.id = "popover-fix";
-  
-    //append the replacement popover to the document and remove the original one
-    document.body.appendChild(popover_new);
-    document.body.removeChild(popover_old);
-  }
-  
-  window.addEventListener('load', (e) => {
-    //grab all of the usernames on the page and create event listeners for them 
-    var usernames = document.querySelectorAll("a[data-bs-toggle=popover]");
-    usernames.forEach(popoverEventListener);
-  
-    //create an event listener to check if a clicked element is a username
-    document.addEventListener("click", function(e){closePopover(e)});
-  });
-  
+function eventasdf(value){
+	var content_id = value.getAttributeNode("data-content-id").value;
+	value.addEventListener("click", function(){jhkj(content_id)});
+}
+
+function checkIfBussy(){
+	if (document.getElementById("bussy") != null){
+		document.body.removeChild(document.getElementById("bussy"));
+	}
+}
+
+function dfgh(e){
+	active = document.activeElement;
+	if (active.getAttributeNode("class") == null || active.getAttributeNode("class").nodeValue != "user-name text-decoration-none"){
+		checkIfBussy();
+	}
+}
+
+function jhkj(value){
+	checkIfBussy();
+	var popover_shit = document.getElementsByClassName("popover")[0];
+	var uiop = document.createElement("DIV");
+
+	uiop.innerHTML = popover_shit.outerHTML;
+	uiop.id = "bussy";
+
+	document.body.appendChild(uiop);
+	document.body.removeChild(popover_shit);
+}
+
+
+var usernames = document.querySelectorAll("a.user-name");
+usernames.forEach(eventasdf);
+
+document.addEventListener("click", function(e){dfgh(e)});
 
 function popclick(author) {
     for (const x of document.getElementsByClassName('pop-banner')) {x.src = author["bannerurl"]}
