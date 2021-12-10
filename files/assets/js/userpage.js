d6849a9f2f87a3da02c611b8c07f5b0e082635a1
Aevann1 <randomname42029@gmail.com>
2021-12-10 18:36:08 +0200

    fsdfds

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -4,27 +4,26 @@ if (uid)
 {
 	uid = uid.innerHTML
 
+	let audio = new Audio(`/songs/${uid}`);
+	audio.loop=true;
+
+	function pause() {
+		audio.pause();
+		document.getElementById("pause1").classList.toggle("d-none");
+		document.getElementById("play1").classList.toggle("d-none");
+		document.getElementById("pause2").classList.toggle("d-none");
+		document.getElementById("play2").classList.toggle("d-none");
+	}
+
+	function play() {
+		audio.play();
+		document.getElementById("pause1").classList.toggle("d-none");
+		document.getElementById("play1").classList.toggle("d-none");
+		document.getElementById("pause2").classList.toggle("d-none");
+		document.getElementById("play2").classList.toggle("d-none");
+	}
+
 	window.addEventListener('load', (e) => {
-
-		let audio = new Audio(`/songs/${uid}`);
-		audio.loop=true;
-
-		function pause() {
-			audio.pause();
-			document.getElementById("pause1").classList.toggle("d-none");
-			document.getElementById("play1").classList.toggle("d-none");
-			document.getElementById("pause2").classList.toggle("d-none");
-			document.getElementById("play2").classList.toggle("d-none");
-		}
-	
-		function play() {
-			audio.play();
-			document.getElementById("pause1").classList.toggle("d-none");
-			document.getElementById("play1").classList.toggle("d-none");
-			document.getElementById("pause2").classList.toggle("d-none");
-			document.getElementById("play2").classList.toggle("d-none");
-		}
-	
 		audio.play();
 		document.getElementById('userpage').addEventListener('click', () => {
 			if (audio.paused) audio.play(); 
