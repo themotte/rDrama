a8476aed592480e342815e5893968e36b2667779
kek7198 <grayboy99@protonmail.com>
2021-12-10 14:41:54 -0600

    fix

--- files/assets/js/popover.js
+++ files/assets/js/popover.js
@@ -1,31 +1,35 @@
-function eventasdf(value) {
+function eventasdf(value){
     var content_id = value.getAttributeNode("data-content-id").value;
-    value.addEventListener("click", function() {
-        jhkj(content_id)
-    });
+    value.addEventListener("click", function(){jhkj(content_id)});
 }
-function checkIfBussy() {
-    if (document.getElementById("bussy") != null) {
+
+function checkIfBussy(){
+    if (document.getElementById("bussy") != null){
         document.body.removeChild(document.getElementById("bussy"));
     }
 }
-function dfgh(e) {
+
+function dfgh(e){
     active = document.activeElement;
-    if (active.getAttributeNode("class") == null || active.getAttributeNode("class").nodeValue != "user-name") {
+    if (active.getAttributeNode("class") == null || active.getAttributeNode("class").nodeValue != "user-name"){
         checkIfBussy();
     }
 }
-function jhkj(value) {
+
+function jhkj(value){
     checkIfBussy();
     var popover_shit = document.getElementsByClassName("popover")[0];
     var uiop = document.createElement("DIV");
+
     uiop.innerHTML = popover_shit.outerHTML;
     uiop.id = "bussy";
+
     document.body.appendChild(uiop);
     document.body.removeChild(popover_shit);
 }
+
+
 var usernames = document.querySelectorAll("a.user-name");
 usernames.forEach(eventasdf);
-document.addEventListener("click", function(e) {
-    dfgh(e)
-});
+
+document.addEventListener("click", function(e){dfgh(e)});
