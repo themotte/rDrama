69834064d34fc21a817459bf5e2b930e6b1e0e1b
kek7198 <grayboy99@protonmail.com>
2021-12-11 00:25:26 -0600

    fix

---
+++ files/assets/js/christmas/mariah.js
@@ -0,0 +1,18 @@
+const audio = new Audio('/assets/christmas/songs/mariah.mp3');
+audio.loop=true;
+
+function play() {
+		audio.play();
+}
+
+console.log(audio)
+
+audio.loop=true;
+
+window.addEventListener('load', function() {
+	audio.play();
+	document.getElementById('thread').addEventListener('click', () => {
+		if (audio.paused) audio.play(); 
+	}, {once : true});
+
+});
---
+++ files/assets/js/christmas/playlist.js
@@ -0,0 +1,54 @@
+const trackList = [
+'low',
+'bells',
+'bows',
+'buble',
+'carol',
+'cold',
+'deck',
+'elton',
+'frosty',
+'grinch',
+'hark',
+'holiday',
+'kissing',
+'mariah',
+'navidad',
+'nightcore',
+'nightcore2',
+'nightcore3',
+'nightmare',
+'pentatonix',
+'rockin',
+'rudolph',
+'santa',
+'santababy',
+'simply',
+'sinatra',
+'sinatra2',
+'steve',
+'trans',
+'waitresses',
+'wham',
+'wonderland',
+];
+const selected = trackList[Math.floor(Math.random() * trackList.length)];
+
+const audio = new Audio(`/assets/christmas/songs/${selected}.mp3',`);
+audio.loop=true;
+
+function play() {
+		audio.play();
+}
+
+console.log(audio)
+
+audio.loop=true;
+
+window.addEventListener('load', function() {
+	audio.play();
+	document.getElementById('thread').addEventListener('click', () => {
+		if (audio.paused) audio.play(); 
+	}, {once : true});
+
+});
