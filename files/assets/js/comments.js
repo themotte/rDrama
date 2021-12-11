a69cb4af63cd0010a821ea6c537d5f51ac6e9d88
kek7198 <grayboy99@protonmail.com>
2021-12-10 19:44:00 -0600

    fix

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -27,3 +27,25 @@ function collapse_comment(comment_id) {
         window.scrollBy(0, - 100)
     }
 };
+
+function morecomments(cid) {
+    btn = document.getElementById(`btn-${cid}`);
+    btn.disabled = true;
+    btn.innerHTML = "Requesting...";
+    var form = new FormData();
+    form.append("formkey", formkey());
+    var xhr = new XMLHttpRequest();
+    xhr.open("post", `/morecomments/${cid}`);
+    xhr.withCredentials=true;
+    xhr.onload=function(){
+        if (xhr.status==200) {
+            document.getElementById(`morecomments-${cid}`).innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, '');
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+            tooltipTriggerList.map(function(element){
+                return new bootstrap.Tooltip(element);
+            });
+            popovertrigger()
+        }
+    }
+    xhr.send(form)
+}
--- files/assets/js/viewmore.js
+++ files/assets/js/viewmore.js
@@ -19,3 +19,25 @@ function loadMore(pid,sort,offset,id,trigger) {
     }
     xhr.send(form)
 }
+
+function loadMoreReplies(pid,sort,offset,id,trigger) {
+    const btn = document.getElementById(trigger) // trigger button
+    const el = document.getElementById(id) // target element to populate
+    const form = new FormData();
+    const xhr = new XMLHttpRequest();
+
+    btn.classList.toggle('animate-pulse');
+
+    xhr.open("post", `/morecomments/${cid}`);
+    xhr.withCredentials=true;
+    xhr.onload=function(){
+        if (xhr.status==200) {
+            el.innerHTML = xhr.response.replace(/data-src/g, 'src').replace(/data-cfsrc/g, 'src').replace(/style="display:none;visibility:hidden;"/g, ''); // replace desired element with response html
+            btn.style.display = "none"; // hide button
+            initializeBootstrap()
+        } else {
+            btn.disabled = false; // enable our button if GET fails
+        }
+    }
+    xhr.send(form)
+}
