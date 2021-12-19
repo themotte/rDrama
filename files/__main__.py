d2772d5a4af775a4ad60d21525abc9bc1363f94c
Aevann1 <randomname42029@gmail.com>
2021-12-19 14:35:18 +0200

    fdsfds

--- files/__main__.py
+++ files/__main__.py
@@ -16,8 +16,6 @@ import gevent
 from werkzeug.middleware.proxy_fix import ProxyFix
 import redis
 
-if int(environ.get("CHRISTMAS", 0)): templates = 'templates/CHRISTMAS'
-else: templates = 'templates'
 app = Flask(__name__, template_folder=templates)
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=3)
@@ -35,7 +33,7 @@ app.config['DATABASE_URL'] = environ.get("DATABASE_URL")
 app.config['SECRET_KEY'] = environ.get('MASTER_KEY')
 app.config["SERVER_NAME"] = environ.get("DOMAIN").strip()
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 86400
-app.config["SESSION_COOKIE_NAME"] = "session_" + environ.get("SITE_NAME").strip().lower()
+app.config["SESSION_COOKIE_NAME"] = "session_" + environ.get("DOMAIN")
 app.config["VERSION"] = "1.0.0"
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 app.config["SESSION_COOKIE_SECURE"] = bool(int(environ.get("FORCE_HTTPS", 1)))
