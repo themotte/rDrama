6728f1092b300cd13b82838071935e1152c52e25
Aevann1 <randomname42029@gmail.com>
2021-12-20 00:47:51 +0200

    dfsfsd

--- files/classes/user.py
+++ files/classes/user.py
@@ -40,7 +40,7 @@ class User(Base):
 	customtitleplain = deferred(Column(String))
 	titlecolor = Column(String, default=defaultcolor)
 	theme = Column(String, default=defaulttheme)
-	theme2 = Column(String, default=defaulttheme)
+	theme2 = Column(String, default='theme-iron dark')
 	themecolor = Column(String, default=defaultcolor)
 	cardview = Column(Boolean, default=cardview)
 	oldsite = Column(Boolean)
