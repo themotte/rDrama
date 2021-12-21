973bc94b2418e6806212bead70005b1e70784e03
Aevann1 <randomname42029@gmail.com>
2021-12-21 21:58:38 +0200

    dfsfsd

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -689,7 +689,7 @@ def settings_security_post(v):
 									   v=v)
 				  )
 
-		return render_template("settings_security.html", v=v, error="Check your email and click the verification link to complete the email change."))
+		return render_template("settings_security.html", v=v, error="Check your email and click the verification link to complete the email change.")
 
 	if request.values.get("2fa_token", ""):
 
