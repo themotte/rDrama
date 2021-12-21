a177c0597239cce76172b6b1afe8ba223a206458
Aevann1 <randomname42029@gmail.com>
2021-12-21 22:36:48 +0200

    sdffsd

--- files/classes/user.py
+++ files/classes/user.py
@@ -300,8 +300,6 @@ class User(Base):
 	@lazy
 	def formkey(self):
 
-		if "session_id" not in session: session["session_id"] = token_hex(52)
-
 		msg = f"{session['session_id']}+{self.id}+{self.login_nonce}"
 
 		return generate_hash(msg)
