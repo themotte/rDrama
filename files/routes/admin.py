136f7eb4553e0879b1e24fecc63be869e32eb385
Aevann1 <randomname42029@gmail.com>
2021-12-23 20:42:06 +0200

    dfs

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -59,7 +59,7 @@ def distribute(v, cid):
 
 	cid = notif_comment(f"You lost the 200 coins you bet on [{post.permalink}]({post.permalink}) :marseylaugh:")
 	cids = [x.id for x in post.bet_options]
-	cids.pop(cid)
+	cids.remove(cid)
 	votes = g.db.query(CommentVote).filter(CommentVote.comment_id.in_(cids)).all()
 	for vote in votes: add_notif(cid, vote.user.id)
 
