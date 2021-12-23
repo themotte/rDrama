39a6db7f4ea6f3c0ed69cebf8b99a338feda73b5
Aevann1 <randomname42029@gmail.com>
2021-12-23 20:36:10 +0200

    dsfsdf

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -57,7 +57,7 @@ def distribute(v, cid):
 	g.db.add(autobetter)
 
 	cid = notif_comment(f"You lost the 200 coins you bet on [{post.permalink}]({post.permalink}) :marseylaugh:")
-	cids = (x.id for x in post.bet_options)
+	cids = (x.id for x in post.bet_options if x.id != cid)
 	votes = g.db.query(CommentVote).filter(CommentVote.comment_id.in_(cids)).all()
 	for vote in votes: add_notif(cid, vote.user.id)
 
