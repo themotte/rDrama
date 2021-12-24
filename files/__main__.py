92139758470c814034dcc1e0e5c1d32566d3a24f
Aevann1 <randomname42029@gmail.com>
2021-12-24 05:20:36 +0200

    sfd

--- files/__main__.py
+++ files/__main__.py
@@ -59,14 +59,14 @@ app.config["RATELIMIT_DEFAULTS_EXEMPT_WHEN"]=lambda:False
 app.config["RATELIMIT_HEADERS_ENABLED"]=True
 app.config["CACHE_TYPE"] = "filesystem"
 app.config["CACHE_DIR"] = "cache"
-app.config["RATELIMIT_STORAGE_URL"] = "redis://redis"
+app.config["RATELIMIT_STORAGE_URL"] = "redis://localhost"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = environ.get("MAIL_USERNAME", "").strip()
 app.config['MAIL_PASSWORD'] = environ.get("MAIL_PASSWORD", "").strip()
 
-r=redis.Redis(host="redis://redis", decode_responses=True, ssl_cert_reqs=None)
+r=redis.Redis(host="redis://localhost", decode_responses=True, ssl_cert_reqs=None)
 
 limiter = Limiter(
 	app,
