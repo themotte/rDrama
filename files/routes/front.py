57153eb9722527a2c8f94aec7b14c449d70cc338
Aevann1 <randomname42029@gmail.com>
2021-12-24 03:30:48 +0200

    dsf

--- files/routes/front.py
+++ files/routes/front.py
@@ -125,6 +125,9 @@ def notifications(v):
 @auth_desired
 def front_all(v):
 
+	session.permanent = True
+	if not session.get("session_id"): session["session_id"] = secrets.token_hex(49)
+
 	if not v and request.path == "/" and not request.headers.get("Authorization"): return redirect(f"/logged_out{request.full_path}")
 
 	if v and v.is_banned and not v.unban_utc: return render_template('errors/500.html', error=True, v=v), 500
