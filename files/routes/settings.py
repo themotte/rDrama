c9b2f284e8c7207cb20594cdf3a93301d51795e4
Aevann1 <randomname42029@gmail.com>
2021-12-30 17:20:09 +0200

    sfd

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -364,6 +364,8 @@ def settings_profile_post(v):
 
 	theme = request.values.get("theme")
 	if theme:
+		if theme == "transparent" and not v.background: 
+			return render_template(f"{template}settings_profile.html", v=v, error="You need to set a background to use the transparent theme!")
 		v.theme = theme
 		if theme == "win98": v.themecolor = "30409f"
 		updated = True
