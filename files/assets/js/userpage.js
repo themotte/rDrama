072fe7aad5fd79d99dbfcc7080d251ebec816f2c
Aevann1 <randomname42029@gmail.com>
2021-12-30 18:54:24 +0200

    fds

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -22,4 +22,11 @@ if (uid)
 		document.getElementById("pause2").classList.toggle("d-none");
 		document.getElementById("play2").classList.toggle("d-none");
 	}
+
+	window.addEventListener('load', (e) => {
+		audio.play();
+		document.getElementById('userpage').addEventListener('click', () => {
+			if (audio.paused) audio.play(); 
+		}, {once : true});
+	});
 }
