470ae9fc6e6999ed2f132b04294c7bb468971d64
Aevann1 <randomname42029@gmail.com>
2022-01-02 16:14:12 +0200

    fds

--- files/routes/static.py
+++ files/routes/static.py
@@ -200,7 +200,7 @@ def log(v):
 
 	actions = g.db.query(ModAction)
 	if not (v and v.admin_level > 1): 
-		actions = actions.filter(ModAction.kind.notin_(["shadowban","unshadowban","check"]))
+		actions = actions.filter(ModAction.kind.notin_(["shadowban","unshadowban"]))
 	
 	if admin_id: actions = actions.filter_by(user_id=admin_id)
 	if kind: actions = actions.filter_by(kind=kind)
