2db2e03ef022e2803de383d0a7fae9c9b8d808a2
kek7198 <grayboy99@protonmail.com>
2022-01-02 22:37:59 -0600

    fix

--- files/assets/CHRISTMAS/js/popover.js
+++ files/assets/CHRISTMAS/js/popover.js
@@ -27,7 +27,7 @@ function userPopover(author) {
         popover_new.getElementsByClassName('pop-badges')[0].innerHTML = badges
         popover_new.getElementsByClassName('pop-uid')[0].innerHTML = author["id"]
 
-        popover_new.getElementsById('popoverMessageButton').setAttribute('data-bs-recipient', 'mdo')
+        popover_new.getElementById('popoverMessageButton').setAttribute('data-bs-recipient', 'mdo')
         console.log(button)
 
         document.body.appendChild(popover_new);
