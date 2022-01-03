f7382751bdda23313fd914f468ceec4be0575ba5
kek7198 <grayboy99@protonmail.com>
2022-01-02 19:06:23 -0600

    fix

--- files/assets/CHRISTMAS/js/popover.js
+++ files/assets/CHRISTMAS/js/popover.js
@@ -31,18 +31,21 @@ function userPopover(author) {
     for (const x of author["badges"]) {
         badges += `<img alt="badge" width="32" loading="lazy" src="${x}">`
     }
-    popover_new.getElementsByClassName('pop-banner')[0].src = author["bannerurl"]
-    popover_new.getElementsByClassName('pop-picture')[0].src = author["profile_url"]
-    popover_new.getElementsByClassName('pop-username')[0].innerHTML = author["username"]
-    popover_new.getElementsByClassName('pop-bio')[0].innerHTML = author["bio_html"]
-    popover_new.getElementsByClassName('pop-postcount')[0].innerHTML = author["post_count"]
-    popover_new.getElementsByClassName('pop-commentcount')[0].innerHTML = author["comment_count"]
-    popover_new.getElementsByClassName('pop-coins')[0].innerHTML = author["coins"]
-    popover_new.getElementsByClassName('pop-viewmore')[0].href = author["url"]
-    popover_new.getElementsByClassName('pop-badges')[0].innerHTML = badges
-  
-    document.body.appendChild(popover_new);
-    document.body.removeChild(popover_old);
+
+    if (popover_old) {
+        popover_new.getElementsByClassName('pop-banner')[0].src = author["bannerurl"]
+        popover_new.getElementsByClassName('pop-picture')[0].src = author["profile_url"]
+        popover_new.getElementsByClassName('pop-username')[0].innerHTML = author["username"]
+        popover_new.getElementsByClassName('pop-bio')[0].innerHTML = author["bio_html"]
+        popover_new.getElementsByClassName('pop-postcount')[0].innerHTML = author["post_count"]
+        popover_new.getElementsByClassName('pop-commentcount')[0].innerHTML = author["comment_count"]
+        popover_new.getElementsByClassName('pop-coins')[0].innerHTML = author["coins"]
+        popover_new.getElementsByClassName('pop-viewmore')[0].href = author["url"]
+        popover_new.getElementsByClassName('pop-badges')[0].innerHTML = badges
+      
+        document.body.appendChild(popover_new);
+        document.body.removeChild(popover_old);
+    }
 }
 
 document.addEventListener("click", function(){
