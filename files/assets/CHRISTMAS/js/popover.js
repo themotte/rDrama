3b2096de0c5d199f79a447bbc3933d41f3c666ed
kek7198 <grayboy99@protonmail.com>
2022-01-02 20:04:45 -0600

    fix

--- files/assets/CHRISTMAS/js/popover.js
+++ files/assets/CHRISTMAS/js/popover.js
@@ -35,7 +35,6 @@ function userPopover(author) {
     if (popover_old) {
         popover_new.getElementsByClassName('pop-banner')[0].src = author["bannerurl"]
         popover_new.getElementsByClassName('pop-picture')[0].src = author["profile_url"]
-        popover_new.getElementsByClassName('pop-uid')[0].src = author["id"]
         popover_new.getElementsByClassName('pop-username')[0].innerHTML = author["username"]
         popover_new.getElementsByClassName('pop-bio')[0].innerHTML = author["bio_html"]
         popover_new.getElementsByClassName('pop-postcount')[0].innerHTML = author["post_count"]
@@ -43,6 +42,7 @@ function userPopover(author) {
         popover_new.getElementsByClassName('pop-coins')[0].innerHTML = author["coins"]
         popover_new.getElementsByClassName('pop-viewmore')[0].href = author["url"]
         popover_new.getElementsByClassName('pop-badges')[0].innerHTML = badges
+        popover_new.getElementsByClassName('pop-uid')[0].src = author["id"]
   
         document.body.appendChild(popover_new);
         document.body.removeChild(popover_old);
