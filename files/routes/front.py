ea4edf8ce33124fadcc4c28cd4958ae9e5d48e37
Aevann1 <randomname42029@gmail.com>
2022-01-06 19:37:21 +0200

    fg

--- files/routes/front.py
+++ files/routes/front.py
@@ -29,7 +29,7 @@ def notifications(v):
 	modmail = request.values.get('modmail')
 	posts = request.values.get('posts')
 	if modmail and v.admin_level > 1:
-		comments = g.db.query(Comment).distinct(Comment.parent_comment_id).filter(Comment.sentto==0, not_(Comment.child_comments.any())).order_by(Comment.parent_comment_id.desc()).offset(25*(page-1)).limit(26).all()
+		comments = g.db.query(Comment).filter(Comment.sentto==0).order_by(Comment.id.desc()).offset(25*(page-1)).limit(26).all()
 		next_exists = (len(comments) > 25)
 		comments = comments[:25]
 	elif messages:
