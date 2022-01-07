cf7f5385c12b3e694e431b23f490ddad50fecbe4
Aevann1 <randomname42029@gmail.com>
2022-01-08 01:03:50 +0200

    cx

--- files/helpers/markdown.py
+++ files/helpers/markdown.py
@@ -39,9 +39,7 @@ class CustomRenderer(HTMLRenderer):
 
 		user = get_user(target, graceful=True)
 
-		if g.v.admin_level == 0 and g.v.any_block_exists(user): return f"{space}@{target}"
-
-		if not user: return f"{space}@{target}"
+		if not user or g.v.admin_level == 0 and g.v.any_block_exists(user): return f"{space}@{target}"
 
 		return f'''{space}<a href="/id/{user.id}"><img alt="@{user.username}'s profile picture" loading="lazy" src="/uid/{user.id}/pic" class="pp20">@{user.username}</a>'''
 			
@@ -68,9 +66,7 @@ class Renderer(HTMLRenderer):
 
 		user = get_user(target, graceful=True)
 
-		if g.v.admin_level == 0 and g.v.any_block_exists(user): return f"{space}@{target}"
-
-		if not user: return f"{space}@{target}"
+		if not user or g.v.admin_level == 0 and g.v.any_block_exists(user): return f"{space}@{target}"
 
 		return f'{space}<a href="/id/{user.id}">@{user.username}</a>'
 			
