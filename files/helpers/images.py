1f6f8680576a8544a848a76d926e954508b48e5b
Aevann1 <randomname42029@gmail.com>
2022-01-09 19:27:44 +0200

    bb

--- files/helpers/images.py
+++ files/helpers/images.py
@@ -21,7 +21,7 @@ def process_image(filename=None, resize=False):
 		om.info = i.info
 		om.save(filename, format="WEBP", save_all=True, append_images=list(frames), loop=0)
 	elif i.format.lower() != "webp":
-		if i.format.lower() == "gif": gifwebp(input_image=filename, output_image=filename, option="-q 80")
+		if i.format.lower() == "gif": gifwebp(input_image=filename, output_image=filename, option="-q 100")
 		else: i.save(filename, format="WEBP")
 
 	return f'/static{filename}'
