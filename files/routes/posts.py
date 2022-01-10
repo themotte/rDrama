d7ba907f66f46cbd9ccebfd1e33f8f5d693c9917
Aevann1 <randomname42029@gmail.com>
2022-01-11 01:55:19 +0200

    bbb

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -556,24 +556,21 @@ def thumbnail_thread(pid):
 
 	cpu = cpu_percent()
 
-	if cpu > 7:
+	if cpu > 70:
 		print('wtf', flush=True)
-		# with open('under_attack', 'r') as f: content = f.read()
+		with open('under_attack', 'r') as f: content = f.read()
 
-		# with open('under_attack', 'w') as f:
-		# 	if content != "yes":
-		# 		f.write("yes")
-		# 		ma = ModAction(
-		# 			kind="enable_under_attack",
-		# 			user_id=AUTOJANNY_ID,
-		# 		)
-		# 		g.db.add(ma)
-		# 		g.db.commit()
-		# 		data='{"value":"under_attack"}'
-
-		# 		response = str(requests.patch(f'https://api.cloudflare.com/client/v4/zones/{CF_ZONE}/settings/security_level', headers=CF_HEADERS, data=data))
-		# 		if response == "<Response [200]>": return {"message": "Under attack mode enabled!"}
-		# 		return {"error": "Failed to enable under attack mode."}
+		with open('under_attack', 'w') as f:
+			if content != "yes":
+				f.write("yes")
+				ma = ModAction(
+					kind="enable_under_attack",
+					user_id=AUTOJANNY_ID,
+				)
+				g.db.add(ma)
+				g.db.commit()
+				data='{"value":"under_attack"}'
+				requests.patch(f'https://api.cloudflare.com/client/v4/zones/{CF_ZONE}/settings/security_level', headers=CF_HEADERS, data=data)
 
 	def expand_url(post_url, fragment_url):
 
