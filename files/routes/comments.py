49054092807308a5f8ed620eed8264281bd8b5e0
Aevann1 <randomname42029@gmail.com>
2022-01-11 19:42:56 +0200

    bb

--- files/routes/comments.py
+++ files/routes/comments.py
@@ -51,7 +51,7 @@ def post_pid_comment_cid(cid, pid=None, anything=None, v=None):
 
 	if comment.post and comment.post.club and not (v and (v.paid_dues or v.id in [comment.author_id, comment.post.author_id])): abort(403)
 
-	if not comment.parent_submission and not (v and (comment.author.id == v.id or comment.sentto == v.id)): abort(403)
+	if not comment.parent_submission and not (v and (comment.author.id == v.id or comment.sentto == v.id)) and not (v and v.admin_level > 1) : abort(403)
 	
 	if not pid:
 		if comment.parent_submission: pid = comment.parent_submission
