0d723a9c437243622c6d53e5f5894b8152abcd31
Aevann1 <randomname42029@gmail.com>
2022-01-18 18:19:49 +0200

    fd

--- files/routes/users.py
+++ files/routes/users.py
@@ -368,13 +368,21 @@ def leaderboard(v):
 	except: pos12 = None
 	userss12 = users12[:25]
 
+	if users13 == None:
+		pos13 = None
+		userss13 = None
+	else:
+		pos13 = [x[0] for x in users13].index(v.username.lower())
+		pos13 = (pos13, users13[pos13][1])
+		userss13 = users13[:25]
+
 	try:
 		pos15 = [x[0].id for x in users15].index(v.id)
 		pos15 = (pos15, users15[pos15][1])
 	except: pos15 = None
 	userss15 = users15[:25]
 
-	return render_template("leaderboard.html", v=v, users1=users1, pos1=pos1, users2=users2, pos2=pos2, users3=users3, pos3=pos3, users4=users4, pos4=pos4, users5=users5, pos5=pos5, users6=users6, pos6=pos6, users7=users7, pos7=pos7, users9=userss9, pos9=pos9, users10=users10, pos10=pos10, users12=userss12, pos12=pos12, users13=users13, users15=userss15, pos15=pos15)
+	return render_template("leaderboard.html", v=v, users1=users1, pos1=pos1, users2=users2, pos2=pos2, users3=users3, pos3=pos3, users4=users4, pos4=pos4, users5=users5, pos5=pos5, users6=users6, pos6=pos6, users7=users7, pos7=pos7, users9=userss9, pos9=pos9, users10=users10, pos10=pos10, users12=userss12, pos12=pos12, users13=userss13, pos13=pos13, users15=userss15, pos15=pos15)
 
 @app.get("/@<username>/css")
 @auth_required
