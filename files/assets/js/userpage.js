f07bd552ccd0513c42ed53921e1d9069c39c9d33
Aevann1 <randomname42029@gmail.com>
2022-01-18 15:40:45 +0200

    fds

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -7,20 +7,9 @@ if (uid)
 	let audio = new Audio(`/songs/${uid}`);
 	audio.loop=true;
 
-	function pause() {
-		audio.pause();
-		document.getElementById("pause1").classList.toggle("d-none");
-		document.getElementById("play1").classList.toggle("d-none");
-		document.getElementById("pause2").classList.toggle("d-none");
-		document.getElementById("play2").classList.toggle("d-none");
-	}
-
-	function play() {
-		audio.play();
-		document.getElementById("pause1").classList.toggle("d-none");
-		document.getElementById("play1").classList.toggle("d-none");
-		document.getElementById("pause2").classList.toggle("d-none");
-		document.getElementById("play2").classList.toggle("d-none");
+	function toggle() {
+		if (audio.paused) audio.play()
+		else audio.pause()
 	}
 
 	window.addEventListener('load', (e) => {
