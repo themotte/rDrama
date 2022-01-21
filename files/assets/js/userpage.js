600aae84e2caf9d12f020a20bbcfd97021fb89b2
Aevann1 <randomname42029@gmail.com>
2022-01-22 00:53:31 +0200

    vxc

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -1,29 +1,29 @@
 const playing = localStorage.getItem("playing")
 
-if (!playing)
+let uid = document.getElementById('uid')
+
+if (uid)
 {
-	let uid = document.getElementById('uid')
+	uid = uid.innerHTML
 
-	if (uid)
-	{
-		uid = uid.innerHTML
+	let audio = new Audio(`/songs/${uid}`);
+	audio.loop=true;
 
-		let audio = new Audio(`/songs/${uid}`);
-		audio.loop=true;
-
-		function toggle() {
-			if (audio.paused)
-			{
-				audio.play()
-				localStorage.setItem("playing", "1")
-			}
-			else
-			{
-				audio.pause()
-				localStorage.setItem("playing", "")
-			}
+	function toggle() {
+		if (audio.paused)
+		{
+			audio.play()
+			localStorage.setItem("playing", "1")
 		}
+		else
+		{
+			audio.pause()
+			localStorage.setItem("playing", "")
+		}
+	}
 
+	if (!playing)
+	{
 		audio.play();
 		document.getElementById('userpage').addEventListener('click', () => {
 			if (audio.paused) audio.play(); 
@@ -34,45 +34,45 @@ if (!playing)
 			localStorage.setItem("playing", "")
 		});
 	}
-	else
+}
+else
+{
+	let uid = document.getElementById('vid')
+	if (uid)
 	{
-		let uid = document.getElementById('vid')
-		if (uid)
-		{
-			uid = uid.innerHTML
+		uid = uid.innerHTML
 
-			const paused = localStorage.getItem("paused")
+		const paused = localStorage.getItem("paused")
 
-			let audio = new Audio(`/songs/${uid}`);
-			audio.loop=true;
-		
-			function toggle() {
-				if (audio.paused)
-				{
-					audio.play()
-					localStorage.setItem("paused", "")
-					localStorage.setItem("playing", "1")
-				}
-				else
-				{
-					audio.pause()
-					localStorage.setItem("paused", "1")
-					localStorage.setItem("playing", "")
-				}
-			}
-		
-			if (!paused)
+		let audio = new Audio(`/songs/${uid}`);
+		audio.loop=true;
+	
+		function toggle() {
+			if (audio.paused)
 			{
-				audio.play();
-				document.getElementById('userpage').addEventListener('click', () => {
-					if (audio.paused) audio.play(); 
-				}, {once : true});
-
+				audio.play()
+				localStorage.setItem("paused", "")
 				localStorage.setItem("playing", "1")
-				window.addEventListener("unload", function(e) {
-					localStorage.setItem("playing", "")
-				});
 			}
+			else
+			{
+				audio.pause()
+				localStorage.setItem("paused", "1")
+				localStorage.setItem("playing", "")
+			}
+		}
+	
+		if (!paused && !playing)
+		{
+			audio.play();
+			document.getElementById('userpage').addEventListener('click', () => {
+				if (audio.paused) audio.play(); 
+			}, {once : true});
+
+			localStorage.setItem("playing", "1")
+			window.addEventListener("unload", function(e) {
+				localStorage.setItem("playing", "")
+			});
 		}
 	}
 }
