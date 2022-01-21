cc7136874e3291ddbaa40399e384e52d252e7471
Aevann1 <randomname42029@gmail.com>
2022-01-22 01:50:02 +0200

    fdssfd

--- files/helpers/images.py
+++ files/helpers/images.py
@@ -5,6 +5,7 @@ import time
 def process_image(file=None, filename=None, resize=0):
 	
 	if not filename: filename = f'/images/{time.time()}'.replace('.','')[:-5] + '.webp'
+	file.save(filename)
 
 	i = IImage.open(file)
 
@@ -25,9 +26,7 @@ def process_image(file=None, filename=None, resize=0):
 		om.save(filename, format="WEBP", save_all=True, append_images=list(frames), loop=0, method=6, allow_mixed=True)
 	elif i.format.lower() != "webp":
 		if i.format.lower() == "gif":
-			file.save(filename)
 			gifwebp(input_image=filename, output_image=filename, option="-mixed -metadata none -f 100 -mt -m 6")
 		else: i.save(filename, format="WEBP", method=6)
-	else: file.save(filename)
 
 	return f'/static{filename}'
