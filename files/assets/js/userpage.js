8d7cfa173a57fce64e175f23ba8fe5c3e7353704
Aevann1 <randomname42029@gmail.com>
2022-01-21 23:59:27 +0200

    fds

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -17,3 +17,37 @@ if (uid)
 		if (audio.paused) audio.play(); 
 	}, {once : true});
 }
+else
+{
+	let uid = document.getElementById('vid')
+	if (uid)
+	{
+		uid = uid.innerHTML
+
+		const paused = localStorage.getItem("paused")
+
+		let audio = new Audio(`/songs/${uid}`);
+		audio.loop=true;
+	
+		function toggle() {
+			if (audio.paused)
+			{
+				audio.play()
+				localStorage.setItem("paused", "")
+			}
+			else
+			{
+				audio.pause()
+				localStorage.setItem("paused", "1")
+			}
+		}
+	
+		if (!paused)
+		{
+			audio.play();
+			document.getElementById('userpage').addEventListener('click', () => {
+				if (audio.paused) audio.play(); 
+			}, {once : true});
+		}
+	}
+}
