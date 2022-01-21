2ba4576f2fb1200d2ade6ee619e7006337c929ca
Aevann1 <randomname42029@gmail.com>
2022-01-21 23:18:53 +0200

    fdfd

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -1088,7 +1088,7 @@ def submit_post(v):
 		new_post.voted = 1
 		if 'megathread' in new_post.title.lower(): sort = 'new'
 		else: sort = v.defaultsortingcomments
-		if c: new_post.replies = [c]
+		if len(body_html) < 40000: new_post.replies = [c]
 		return render_template('submission.html', v=v, p=new_post, sort=sort, render_replies=True, offset=0, success=True)
 
 
