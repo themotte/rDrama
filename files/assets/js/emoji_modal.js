679b28c8a38c772d26c7f666911ace9e3c23cce4
Aevann1 <randomname42029@gmail.com>
2022-01-22 21:03:03 +0200

    vcx

--- files/assets/js/emoji_modal.js
+++ files/assets/js/emoji_modal.js
@@ -156,7 +156,7 @@ function loadEmojis(form) {
 				for (const [key, value] of Object.entries(arr2)) {
 					if (str.includes(`'${key}'`)) continue;
 					let realvalue = Object.values(value)
-					realvalue = realvalue[0] + realvalue[1]
+					realvalue = realvalue[0] + realvalue[2]
 					if (key.match(search_bar.value.toLowerCase()) || search_bar.value.toLowerCase().match(key) || realvalue.match(search_bar.value.toLowerCase())) {
 						str += `<button class="btn m-1 px-0 emoji2" onclick="getEmoji('${key}')" data-bs-toggle="tooltip" title=":${key}:" delay:="0"><img loading="lazy" width=50 src="/static/assets/images/emojis/${key}.webp" alt="${key}-emoji"></button>`;
 					}
