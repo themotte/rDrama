3765aa1da814438ec3168078432cfaa164c4d9d5
Aevann1 <randomname42029@gmail.com>
2022-01-22 19:18:37 +0200

    vcxxcv

--- files/assets/js/comments_v.js
+++ files/assets/js/comments_v.js
@@ -73,7 +73,7 @@ function report_commentModal(id, author) {
 function openReplyBox(id) {
 	const element = document.getElementById(id);
 	const textarea = element.getElementsByTagName('textarea')[0]
-    if (window.getSelection) textarea.value = '>' + window.getSelection().toString() + '\n\n'
+    if (window.getSelection) textarea.value = '>' + window.getSelection().toString()
 	element.classList.remove('d-none')
 	textarea.focus()
 }
