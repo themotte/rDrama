980851d63fa264314834ce2f3851b894c96ee381
Aevann1 <randomname42029@gmail.com>
2022-01-24 19:59:14 +0200

    vc

--- files/helpers/images.py
+++ files/helpers/images.py
@@ -35,10 +35,10 @@ def process_image(file=None, filename=None, resize=0):
 
 		om = next(frames)
 		om.info = i.info
-		om.save(filename, format="WEBP", save_all=True, append_images=list(frames), loop=0, method=6, allow_mixed=True, exif=exif)
+		om.save(filename, format="WEBP", save_all=True, append_images=list(frames), loop=0, method=6, allow_mixed=True)
 	elif i.format.lower() != "webp":
 		if i.format.lower() == "gif":
 			gifwebp(input_image=filename, output_image=filename, option="-mixed -metadata none -f 100 -mt -m 6")
-		else: i.save(filename, format="WEBP", method=6, exif=exif)
+		else: i.save(filename, format="WEBP", method=6)
 
 	return f'/static{filename}'
