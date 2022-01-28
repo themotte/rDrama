fa6b4b42d2a76d858505fa46622f3d09211a54a6
Aevann1 <randomname42029@gmail.com>
2022-01-29 01:55:42 +0200

    fds

--- files/helpers/const.py
+++ files/helpers/const.py
@@ -145,6 +145,7 @@ if SITE == 'rdrama.net':
 	JOAN_ID = 28
 	MOOSE_ID = 1904
 	AEVANN_ID = 1
+	HOMO_ID = 147
 	Q_ID = 1480
 	LAWLZ_ID = 3833
 	LLM_ID = 253
@@ -173,6 +174,7 @@ elif SITE == "pcmemes.net":
 	JOAN_ID = 0
 	MOOSE_ID = 0
 	AEVANN_ID = 1
+	HOMO_ID = 0
 	Q_ID = 0
 	LAWLZ_ID = 0
 	LLM_ID = 0
@@ -201,6 +203,7 @@ else:
 	JOAN_ID = 0
 	MOOSE_ID = 0
 	AEVANN_ID = 0
+	HOMO_ID = 0
 	Q_ID = 0
 	LAWLZ_ID = 0
 	LLM_ID = 0
@@ -522,6 +525,7 @@ NOTIFIED_USERS = {
 	'landlet': LLM_ID,
 	'dong': DONGER_ID,
 	'kippy': KIPPY_ID,
+	'the_homocracy': HOMO_ID
 }
 
 FORTUNE_REPLIES = ('<b style="color:#6023f8">Your fortune: Allah Wills It</b>','<b style="color:#d302a7">Your fortune: Inshallah, Only Good Things Shall Come To Pass</b>','<b style="color:#e7890c">Your fortune: Allah Smiles At You This Day</b>','<b style="color:#7fec11">Your fortune: Your Bussy Is In For A Blasting</b>','<b style="color:#43fd3b">Your fortune: You Will Be Propositioned By A High-Tier Twink</b>','<b style="color:#9d05da">Your fortune: Repent, You Have Displeased Allah And His Vengeance Is Nigh</b>','<b style="color:#f51c6a">Your fortune: Reply Hazy, Try Again</b>','<b style="color:#00cbb0">Your fortune: lmao you just lost 100 dramacoin</b>','<b style="color:#2a56fb">Your fortune: Yikes 😬</b>','<b style="color:#0893e1">Your fortune: You Will Be Blessed With Many Black Bulls</b>','<b style="color:#16f174">Your fortune: NEETmax, The Day Is Lost If You Venture Outside</b>','<b style="color:#fd4d32">Your fortune: A Taste Of Jannah Awaits You Today</b>','<b style="color:#bac200">Your fortune: Watch Your Back</b>','<b style="color:#6023f8">Your fortune: Outlook good</b>','<b style="color:#d302a7">Your fortune: Godly Luck</b>','<b style="color:#e7890c">Your fortune: Good Luck</b>','<b style="color:#7fec11">Your fortune: Bad Luck</b>','<b style="color:#43fd3b">Your fortune: Good news will come to you by mail</b>','<b style="color:#9d05da">Your fortune: Very Bad Luck</b>','<b style="color:#00cbb0">Your fortune: ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!</b>','<b style="color:#2a56fb">Your fortune: Better not tell you now</b>','<b style="color:#0893e1">Your fortune: You will meet a dark handsome stranger</b>','<b style="color:#16f174">Your fortune: （　´_ゝ`）ﾌｰﾝ</b>','<b style="color:#fd4d32">Your fortune: Excellent Luck</b>','<b style="color:#bac200">Your fortune: Average Luck</b>')
@@ -531,5 +535,6 @@ REDDIT_NOTIFS = {
 	'aevann': AEVANN_ID,
 	'carpflo': CARP_ID,
 	'carpathianflorist': CARP_ID,
-	'carpathian florist': CARP_ID
+	'carpathian florist': CARP_ID,
+	'the_homocracy': HOMO_ID
 }
