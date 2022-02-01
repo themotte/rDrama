17a08aeb77d458b0414e237a84f5ba02035e7ebd
Aevann1 <randomname42029@gmail.com>
2022-02-01 04:20:25 +0200

    fddf

--- files/assets/js/delete_post_modal.js
+++ files/assets/js/delete_post_modal.js
@@ -1,22 +1,28 @@
 function delete_postModal(id) {
-
-    function delete_post(){	
-
-        this.innerHTML='Deleting post';	
-        this.disabled = true; 
-
-        var url = '/delete_post/' + id
+    document.getElementById("deletePostButton").onclick =  function() {
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
+        xhr.open("POST", `/delete_post/${id}`, true);
         xhr.setRequestHeader('xhr', 'xhr');
         var form = new FormData()
         form.append("formkey", formkey());
-        xhr.onload = function() {location.reload(true);};
+        xhr.onload = function() {
+            let data
+            try {data = JSON.parse(xhr.response)}
+            catch(e) {console.log(e)}
+            if (xhr.status >= 200 && xhr.status < 300 && data && data['message']) {
+                document.getElementById(`post-${id}`).classList.add('deleted');
+                document.getElementById(`delete-${id}`).classList.add('d-none');
+                document.getElementById(`undelete-${id}`).classList.remove('d-none');
+                document.getElementById(`delete2-${id}`).classList.add('d-none');
+                document.getElementById(`undelete2-${id}`).classList.remove('d-none');
+                document.getElementById('toast-post-success-text').innerText = data["message"];
+                new bootstrap.Toast(document.getElementById('toast-post-success')).show();
+            } else {
+                document.getElementById('toast-post-error-text').innerText = "Error, please try again later."
+                if (data && data["error"]) document.getElementById('toast-post-error-text').innerText = data["error"];
+                new bootstrap.Toast(document.getElementById('toast-post-error')).show();
+            }
+        };
         xhr.send(form);
-    }
-
-    document.getElementById("deletePostButton-mobile").onclick = delete_post;
-
-    document.getElementById("deletePostButton").onclick =  delete_post;
-
-};
+    };
+}
