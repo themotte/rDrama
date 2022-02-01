fc080f5f49c5a2775a511d6767001db696693030
Aevann1 <randomname42029@gmail.com>
2022-02-01 06:01:04 +0200

    fds

--- files/assets/js/comments+submission_listing.js
+++ files/assets/js/comments+submission_listing.js
@@ -33,28 +33,31 @@ function popovertrigger() {
 popovertrigger()
 
 function popclick(author) {
-    popover = document.getElementsByClassName("popover")[0]
+    setTimeout(() => {
+        let popover = document.getElementsByClassName("popover")
+        popover = popover[popover.length-1]
 
-    let badges = ''
-    for (const x of author["badges"]) {
-        badges += `<img alt="badge" width="32" loading="lazy" src="${x}">`
-    }
-    popover.getElementsByClassName('pop-banner')[0].src = author["bannerurl"]
-    popover.getElementsByClassName('pop-picture')[0].src = author["profile_url"]
-    popover.getElementsByClassName('pop-username')[0].innerHTML = author["username"]
-    popover.getElementsByClassName('pop-bio')[0].innerHTML = author["bio_html"]
-    popover.getElementsByClassName('pop-postcount')[0].innerHTML = author["post_count"]
-    popover.getElementsByClassName('pop-commentcount')[0].innerHTML = author["comment_count"]
-    popover.getElementsByClassName('pop-coins')[0].innerHTML = author["coins"]
-    popover.getElementsByClassName('pop-viewmore')[0].href = author["url"]
-    popover.getElementsByClassName('pop-badges')[0].innerHTML = badges
+        let badges = ''
+        for (const x of author["badges"]) {
+            badges += `<img alt="badge" width="32" loading="lazy" src="${x}">`
+        }
+        popover.getElementsByClassName('pop-banner')[0].src = author["bannerurl"]
+        popover.getElementsByClassName('pop-picture')[0].src = author["profile_url"]
+        popover.getElementsByClassName('pop-username')[0].innerHTML = author["username"]
+        popover.getElementsByClassName('pop-bio')[0].innerHTML = author["bio_html"]
+        popover.getElementsByClassName('pop-postcount')[0].innerHTML = author["post_count"]
+        popover.getElementsByClassName('pop-commentcount')[0].innerHTML = author["comment_count"]
+        popover.getElementsByClassName('pop-coins')[0].innerHTML = author["coins"]
+        popover.getElementsByClassName('pop-viewmore')[0].href = author["url"]
+        popover.getElementsByClassName('pop-badges')[0].innerHTML = badges
+    ; }, 1);
 }
 
 document.addEventListener("click", function(){
     active = document.activeElement.getAttributeNode("class");
-    if (!(active && active.nodeValue == "user-name text-decoration-none")){
-        let pop = document.getElementByClassName("popover")
-        pop = pop[pop.length-1]
-        if (pop) document.body.removeChild(pop);
+    if (active && active.nodeValue == "user-name text-decoration-none"){
+        pops = document.getElementsByClassName('popover')
+        if (pops.length > 1) pops[0].remove()
     }
+    else document.querySelectorAll('.popover').forEach(e => e.remove());
 });
