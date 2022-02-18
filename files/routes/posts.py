8743a4b1239db8bed5542f444ddec80b8bc198dc
Aevann1 <randomname42029@gmail.com>
2022-02-18 10:34:00 +0200

    vc

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -162,8 +162,6 @@ def post_id(pid, anything=None, v=None, sub=None):
 
 	if post.club and not (v and (v.paid_dues or v.id == post.author_id)): abort(403)
 
-	if post.private and not (v and (v.admin_level > 1 or v.id == post.author.id)): abort(403)
-
 	if v:
 		votes = g.db.query(CommentVote).filter_by(user_id=v.id).subquery()
 
