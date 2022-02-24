7b4d3a7b8f1c53561289ad4f79989e2122c5d773
Aevann1 <randomname42029@gmail.com>
2022-02-24 04:52:46 +0200

    fds

--- files/classes/user.py
+++ files/classes/user.py
@@ -214,7 +214,7 @@ class User(Base):
 	@property
 	@lazy
 	def paid_dues(self):
-		return self.admin_level > 1 or self.club_allowed or self.truecoins > int(environ.get("DUES").strip()) and self.club_allowed != False
+		return self.admin_level or self.club_allowed or self.patron > 1 or (self.patron == 1 and self.truecoins > dues/10) or (self.club_allowed != False and self.truecoins > dues)
 
 	@lazy
 	def any_block_exists(self, other):
