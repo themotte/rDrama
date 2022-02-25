6e2b0a8b7985fd045f062d2ebbe106609c973753
Aevann1 <randomname42029@gmail.com>
2022-02-25 15:26:56 +0200

    fdfd

--- files/classes/user.py
+++ files/classes/user.py
@@ -214,7 +214,7 @@ class User(Base):
 	@property
 	@lazy
 	def paid_dues(self):
-		return self.admin_level or self.club_allowed or self.patron > 1 or (self.patron == 1 and self.truecoins > dues/10) or (self.club_allowed != False and self.truecoins > dues)
+		return not self.shadowbanned and not (self.is_banned and not self.unban_utc) and (self.admin_level or self.club_allowed or self.patron > 1 or (self.patron == 1 and self.truecoins > dues/10) or (self.club_allowed != False and self.truecoins > dues))
 
 	@lazy
 	def any_block_exists(self, other):
