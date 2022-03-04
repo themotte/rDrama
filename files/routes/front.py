759c0c8ccedc89d347c96a2356a5b7124dffae62
Aevann1 <randomname42029@gmail.com>
2022-03-04 22:58:45 +0200

    castle

--- files/routes/front.py
+++ files/routes/front.py
@@ -131,10 +131,10 @@ def notifications(v):
 						   )
 
 
-@app.get("/", subdomain="<subdomain>")
-@app.get("/logged_out", subdomain="<subdomain>")
-@app.get("/s/<sub>", subdomain="<subdomain>")
-@app.get("/logged_out/s/<sub>", subdomain="<subdomain>")
+@app.get("/")
+@app.get("/logged_out")
+@app.get("/s/<sub>")
+@app.get("/logged_out/s/<sub>")
 @limiter.limit("3/second;30/minute;1000/hour;5000/day")
 @auth_desired
 def front_all(v, sub=None, subdomain=None):
