4a0576cd3f1b81a59a5e2cb3cbec8592fe088bcb
Aevann1 <randomname42029@gmail.com>
2022-03-05 23:59:35 +0200

    far

--- files/assets/js/sort_table.js
+++ files/assets/js/sort_table.js
@@ -11,12 +11,12 @@ function sort_table(n) {
 			x = rows[i].getElementsByTagName("TD")[n];
 			y = rows[i + 1].getElementsByTagName("TD")[n];
 			if (dir == "asc") {
-				if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
 					shouldSwitch = true;
 					break;
 				}
 			} else if (dir == "desc") {
-				if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
 					shouldSwitch = true;
 					break;
 				}
