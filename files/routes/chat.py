16fff00ea56ab6cab78487bfe7fb6ed0a376b01d
Aevann1 <randomname42029@gmail.com>
2022-03-20 01:27:39 +0200

    fds

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -5,26 +5,28 @@ from datetime import datetime
 from flask_socketio import SocketIO, emit
 from files.__main__ import app
 from flask import render_template
+import sys
 
-sex = SocketIO(app, async_mode='gevent-websocket')
+if "load_chat" in sys.argv:
+	sex = SocketIO(app, async_mode='gevent-websocket')
 
-@app.get("/chat")
-@auth_required
-def chat( v):
-	return render_template("chat.html", v=v)
+	@app.get("/chat")
+	@auth_required
+	def chat( v):
+		return render_template("chat.html", v=v)
 
 
-@sex.on('speak')
-@auth_required
-def speak(data, v):
+	@sex.on('speak')
+	@auth_required
+	def speak(data, v):
 
-	data={
-		"avatar": v.profile_url,
-		"username":v.username,
-		"text":sanitize(data[:1000].strip()),
-		"time": time.strftime("%d %b %Y at %H:%M:%S", time.gmtime(int(time.time()))),
-		"userlink":v.url
-	}
+		data={
+			"avatar": v.profile_url,
+			"username":v.username,
+			"text":sanitize(data[:1000].strip()),
+			"time": time.strftime("%d %b %Y at %H:%M:%S", time.gmtime(int(time.time()))),
+			"userlink":v.url
+		}
 
-	emit('speak', data)
-	return '', 204
+		emit('speak', data)
+		return '', 204
