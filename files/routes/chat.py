603e460e2b9292a6ab3e16ef6c12002a8be4cf4a
Aevann1 <randomname42029@gmail.com>
2022-03-20 00:27:34 +0200

    fds

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -1,33 +1,12 @@
 import time
-from os import remove
-from PIL import Image as IMAGE
-
 from files.helpers.wrappers import *
-from files.helpers.alerts import *
 from files.helpers.sanitize import *
-from files.helpers.security import *
-from files.helpers.get import *
-from files.helpers.images import *
-from files.helpers.const import *
-from files.classes import *
 from flask import *
-from files.__main__ import app, cache, limiter, db_session
-from .front import frontlist
-from files.helpers.discord import add_role
+from files.__main__ import app, db_session
 from datetime import datetime
-import requests
-from urllib.parse import quote, urlencode
+from flask_socketio import SocketIO
 
-
-
-import sys
-from flask_socketio import *
-
-month = datetime.now().strftime('%B')
-
-sex = SocketIO(
-	app
-	)
+sex = SocketIO(app)
 
 @app.get("/chat")
 @auth_required
@@ -35,75 +14,9 @@ def chat( v):
 	return render_template("chat.html", v=v)
 
 
-SIDS={}
-
-COMMANDS={}
-HELP={}
-
-TYPING={}
-
-
-
-@sex.on('connect')
-def socket_connect_auth_user():
-
-
-	g.db=db_session()
-
-	v=get_logged_in_user()
-
-	if v.id in SIDS:
-		SIDS[v.id].append(request.sid)
-	else:
-		SIDS[v.id]=[request.sid]
-
-
-	emit("status", {'status':"connected"})
-
-	g.db.close()
-
-
-def socket_auth_required(f):
-
-	def wrapper(*args, **kwargs):
-
-		g.db=db_session()
-		v=get_logged_in_user()
-
-		if request.sid not in SIDS.get(v.id, []):
-			if v.id in SIDS:
-				SIDS[v.id].append(request.sid)
-			else:
-				SIDS[v.id]=[request.sid]
-
-		f(*args, v, **kwargs)
-		g.db.close()
-
-
-	wrapper.__name__=f.__name__
-	return wrapper
-
-
-
-@sex.on('join room')
-@socket_auth_required
-def join_guild_room(data, v):
-
-	return True
-
-
-
-
-@sex.on('leave room')
-@socket_auth_required
-def leave_guild_room(data, v):
-	emit("status", {'status':f"Left #{guild.name}"})
-
-
-
 
 @sex.on('speak')
-@socket_auth_required
+@auth_required
 def speak(data, v):
 
 	raw_text=data['text'][0:1000].lstrip().rstrip()
