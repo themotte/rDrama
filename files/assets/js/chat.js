79b31708cfb3c2190554d183df83ad8d6e48b6b4
Aevann1 <randomname42029@gmail.com>
2022-03-20 01:05:32 +0200

    fds

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -7,11 +7,11 @@ document.getElementById('chatsend').onclick = function () {
     document.getElementById('input-text').value = ''
 }
 
-socket.on('speak', function() {
+socket.on('speak', function(json) {
 	console.log(json);
 	document.getElementById('img').src = json['avatar']
 	document.getElementsByClassName('userlink')[0].href = json['userlink']
 	document.getElementsByClassName('username')[0].value = json['username']
 	document.getElementsByClassName('chat-message').innerHTML = json['text']
-	document.getElementById('chat-text').append(document.getElementById('chat-line-template .chat-line').cloneNode())
+	document.getElementById('chat-text').append('fuck')
 })
