148a18409766035b96d10afb930bad9e05e7a5d0
Aevann1 <randomname42029@gmail.com>
2022-03-20 01:24:51 +0200

    fds

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO, emit
 from files.__main__ import app
 from flask import render_template
 
-sex = SocketIO(app, async_mode='gevent')
+sex = SocketIO(app, async_mode='gevent-websocket')
 
 @app.get("/chat")
 @auth_required
