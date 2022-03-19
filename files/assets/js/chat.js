b082d259326fba956c275e837c63a07577a0c2e3
Aevann1 <randomname42029@gmail.com>
2022-03-20 00:55:35 +0200

    fds

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -1,45 +1,22 @@
 let socket=io()
 
-function process_chat(json, template){
+document.getElementById('chatsend').onclick = function () {
+    console.log('clicked')
+    text = document.getElementById('input-text').val()
+    socket.emit('speak', text);
+    document.getElementById('input-text').val('')
+}
+
+socket.on('speak', function() {
 	console.log(json);
 	username=json['username'];
 	text=json['text'];
 	ava=json['avatar']
 
-	var my_name=$('#username').val()
-
-	if (template=="#msg-in" || template=="#wallop" || text.includes('href="/@'+my_name+'"')){
-		$(template+'-template .chat-line').addClass('chat-mention');
-		notifs=notifs+1;
-		if (notifs==1){
-			setTimeout(flash, 500);
-			ding()
-		}
-	}
-	else {
-		$(template+'-template .chat-line').removeClass('chat-mention');
-	};
-
-	should_scroll()
-	$(template+'-template img').attr('src', ava)
-	$(template+'-template img').attr('data-original-title', json['time'])
-	$(template+'-template .userlink').attr('href', json['userlink'])
-	$(template+'-template .username').text(username)
-	$(template+'-template .chat-message').html(text)
-	$('#chat-text').append($(template+'-template .chat-line').clone())
-	scroll()
-}
-
-socket.on('speak', function(json){process_chat(json, "#chat-line")});
-
-socket.on('connect',
-	function(event) {
-		console.log('connected, joining room')
-		name=$('#guildname').val();
-
-		join=($('#autojoin').val()=="True")
-		if (join){
-			socket.emit('join room', {'guild': name });
-		}
-	}
-)
+	document.getElementById('chat-line-template img').attr('src', ava)
+	document.getElementById('chat-line-template img').attr('data-original-title', json['time'])
+	document.getElementById('chat-line-template .userlink').attr('href', json['userlink'])
+	document.getElementById('chat-line-template .username').text(username)
+	document.getElementById('chat-line-template .chat-message').html(text)
+	document.getElementById('chat-text').append(document.getElementById('chat-line-template .chat-line').clone())
+})
