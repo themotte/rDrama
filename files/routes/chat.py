892f1e58492a0bc0a94408d135b4629949d70d58
Aevann1 <randomname42029@gmail.com>
2022-03-20 01:48:43 +0200

    fds

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -14,7 +14,7 @@ def chat( v):
 	return render_template("chat.html", v=v)
 
 
-sex = SocketIO(app, logger=True, engineio_logger=True, debug=True, async_mode='gevent')
+sex = SocketIO(app, logger=True, engineio_logger=True, debug=True)
 
 @sex.on('speak')
 @auth_required
