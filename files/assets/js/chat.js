e432a146795da562325d4b5dfb2cc53bc7e1cb9a
Aevann1 <randomname42029@gmail.com>
2022-03-24 04:21:22 +0200

    chat

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -58,7 +58,7 @@ socket.on('speak', function(json) {
 	document.getElementsByClassName('userlink')[0].href = '/@' + json['username']
 	document.getElementsByClassName('userlink')[0].innerHTML = json['username']
 	document.getElementsByClassName('userlink')[0].style.color = '#' + json['namecolor']
-	document.getElementsByClassName('text')[0].value = text
+	document.getElementsByClassName('text')[0].innerHTML = text
 	document.getElementsByClassName('chat-message')[0].innerHTML = text_html
 	document.getElementById('chat-text').append(document.getElementsByClassName('chat-line')[0].cloneNode(true))
 	if (scrolled_down) box.scrollTo(0, box.scrollHeight)
@@ -77,9 +77,9 @@ function send() {
 	}
 }
 
-function quote(text) {
+function quote(t) {
 	textbox.style.height = '80px'
-	textbox.value = '> ' + text + '\n\n'
+	textbox.value = '> ' + t.previousElementSibling.innerHTML + '\n\n'
 	textbox.focus()
 }
 
