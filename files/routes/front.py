d4170d27693156c5f99c6f9c595ef3d30c72d4c1
Aevann1 <randomname42029@gmail.com>
2022-04-04 01:57:47 +0200

    sfd

--- files/routes/front.py
+++ files/routes/front.py
@@ -104,11 +104,13 @@ def notifications(v):
 
 		print("1: " + str(time.time() - t), flush=True)
 
-		all = [x[0] for x in g.db.query(Notification.comment_id).join(Comment, Notification.comment_id == Comment.id).filter(Comment.is_banned == False,
+		all = g.db.query(Comment.id).join(Notification).filter(
+			Notification.user_id == v.id,
+			Comment.is_banned == False,
 			Comment.deleted_utc == 0,
 			Comment.author_id != AUTOJANNY_ID,
 			Comment.body_html.notlike('<html><body><p>New rdrama mention: <a href="https://old.reddit.com/r/%')
-		).order_by(Comment.top_comment_id.desc()).limit(25).all()]
+		).order_by(Comment.top_comment_id.desc()).offset(25 * (page - 1)).limit(26).all()
 
 		print("2: " + str(time.time() - t), flush=True)
 
