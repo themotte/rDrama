2bbee69641cf9a08286aeacf6aca4aa71245868c
Aevann1 <randomname42029@gmail.com>
2022-04-04 02:24:18 +0200

    fds

--- files/routes/front.py
+++ files/routes/front.py
@@ -135,12 +135,15 @@ def notifications(v):
 					c.replies2 = c.child_comments.filter(or_(Comment.author_id == v.id, Comment.id.in_(all))).all()
 					cids.update(x.id for x in c.replies2)
 					for x in c.replies2:
-						x.replies2 = []
+						if x.replies2 == None: x.replies2 = []
 				while c.parent_comment and (c.parent_comment.author_id == v.id or c.parent_comment.id in all):
 					c = c.parent_comment
 					if c.replies2 == None:
 						c.replies2 = c.child_comments.filter(or_(Comment.author_id == v.id, Comment.id.in_(all))).all()
 						cids.update(x.id for x in c.replies2)
+						for x in c.replies2:
+							if x.replies2 == None: x.replies2 = []
+
 				cids.add(c.id)
 			else:
 				while c.parent_comment:
