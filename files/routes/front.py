dd25d65f867b60774ee2c480a86d19af267ff002
Aevann1 <randomname42029@gmail.com>
2022-04-04 02:01:04 +0200

    fds

--- files/routes/front.py
+++ files/routes/front.py
@@ -110,7 +110,7 @@ def notifications(v):
 			Comment.deleted_utc == 0,
 			Comment.author_id != AUTOJANNY_ID,
 			Comment.body_html.notlike('<html><body><p>New rdrama mention: <a href="https://old.reddit.com/r/%')
-		).order_by(Comment.top_comment_id.desc()).offset(25 * (page - 1)).limit(26).all()]
+		).order_by(Comment.top_comment_id.desc()).all()]
 
 		print("2: " + str(time.time() - t), flush=True)
 
