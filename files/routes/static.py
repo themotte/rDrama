fca942e618eb908a49574c4f45498dee03d05640
Aevann1 <randomname42029@gmail.com>
2022-04-25 21:42:35 +0200

    vxc

--- files/routes/static.py
+++ files/routes/static.py
@@ -198,10 +198,10 @@ def cached_chart(kind):
 	today_cutoff = calendar.timegm(midnight_this_morning)
 
 	if kind == "daily":
-		file = "/daily_chart.png"
+		file = f"{SITE}_daily.png"
 		day_cutoffs = [today_cutoff - 86400 * i for i in range(47)][1:]
 	else:
-		file = "/weekly_chart.png"
+		file = f"{SITE}_weekly.png"
 		day_cutoffs = [today_cutoff - 86400 * 7 * i for i in range(47)][1:]
 
 	day_cutoffs.insert(0, calendar.timegm(now))
