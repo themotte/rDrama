e7e2b901eb5fbc24a8424175a0b7e4672274e118
Aevann1 <randomname42029@gmail.com>
2022-04-26 16:15:06 +0200

    fsd

--- files/classes/submission.py
+++ files/classes/submission.py
@@ -397,10 +397,11 @@ class Submission(Base):
 			if not self.total_poll_voted(v): body += ' d-none'	
 			body += f'"> - <a href="/votes?link=t3_{c.id}"><span id="poll-{c.id}">{c.upvotes}</span> votes</a></span></label></div>'
 
-		curr = self.total_choice_voted(v)
-		if curr: curr = " value=" + str(curr[0].comment_id)
-		else: curr = ''
-		body += f'<input class="d-none" id="current-{self.id}"{curr}>'
+		if self.choices:
+			curr = self.total_choice_voted(v)
+			if curr: curr = " value=" + str(curr[0].comment_id)
+			else: curr = ''
+			body += f'<input class="d-none" id="current-{self.id}"{curr}>'
 
 		for c in self.choices:
 			body += f'''<div class="custom-control"><input name="choice-{self.id}" autocomplete="off" class="custom-control-input" type="radio" id="{c.id}" onchange="choice_vote('{c.id}','{self.id}')"'''
