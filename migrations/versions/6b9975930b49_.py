fc1159c10dc72440c09c85643c24031e8d701a66
faul_sname <faul.sname@gmail.com>
2022-05-11 19:30:29 -0700

    #39 flask db revision --autogenerate now creates an empty migration

--- migrations/versions/6b9975930b49_.py
+++ migrations/versions/6b9975930b49_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 000000000000
+Revision ID: 6b9975930b49
 Revises: 
-Create Date: 2022-05-12 02:08:22.894633
+Create Date: 2022-05-12 02:28:25.780080
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '000000000000'
+revision = '6b9975930b49'
 down_revision = None
 branch_labels = None
 depends_on = None
