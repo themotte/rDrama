f3e8f248e3d256a41e18e66db699c7922da64721
Michael House <mjhouse@protonmail.com>
2022-06-10 06:17:23 -0500

    Added manage.py script

---
+++ manage.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import sys, subprocess, time
+
+def _execute(command,**kwargs):
+    check = kwargs.get('check',False)
+    return subprocess.run(
+        command,
+        check = check,
+        universal_newlines=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    ).stdout
+
+def _docker(command):
+    return _execute([
+        "docker-compose",
+        "exec", '-T',
+        "files",
+        "bash", "-c", 
+        ' && '.join(command)
+    ])
+
+def _running():
+    command = ['docker','container','inspect','-f','{{.State.Status}}','themotte']
+    result = _execute(command).strip()
+    return result == "running"
+
+def _start():
+    command = ['docker-compose','up','--build','-d']
+    result = _execute(command,check=True)
+    time.sleep(1)
+    return result
+
+def _stop():
+    command = ['docker-compose','down']
+    result = _execute(command,check=True)
+    time.sleep(1)
+    return result
+
+def _operation(name, command):
+    # check if running and start if not
+    running = _running()
+
+    if not running:
+        print("Starting containers...")
+        _start()
+
+    # run operation in docker container
+    print(f"Running {name} . . .")
+    print(_docker(command))
+
+    if not running:
+        print("Stopping containers...")
+        _stop()
+
+def run_test(*args):
+    _operation("tests",[
+        "cd service",
+        "FLASK_APP=files/cli:app python3 -m flask db upgrade",
+        "python3 -m pytest -s",
+    ])
+
+def run_migrate(*args):
+    command = 'upgrade'
+
+    if len(args) > 1:
+        command = args[1]
+
+    _operation(command,[
+        "cd service",
+        "export FLASK_APP=files/cli:app",
+        f"python3 -m flask db {command}",
+    ])
+
+def run_help(*args):
+    print("Available commands: (test|migrate|help)")
+    print("Usage: './manage.py <command> [options]'")
+    exit(0)
+
+def error(message,code=1):
+    print(message,file=sys.stderr)
+    exit(code)
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        error("Usage: './manage.py <command> [options]'")
+
+    name = sys.argv[1]
+    args = sys.argv[1:]
+
+    if name == "test":
+        run_test(*args)
+    elif name == "migrate":
+        run_migrate(*args)
+    elif name == "help":
+        run_help(*args)
+    else:
+        error("Not a command")
