4cfd1d5a5b8bc212c96625b5d3681ad3a79009b3
TLSM <104547575+TLSM@users.noreply.github.com>
2022-08-16 23:38:19 -0400

    Fix 2FA modal not displaying in UI (#178). (#180)
    Fixes issue #178. Root cause was attempting to document.getElementById
    of both modal and toggle input before both were loaded into the DOM.
    In the upstream, Cloudflare's Rocket Loader is used, which papers over
    many cases of inattention to loading order.

--- files/assets/js/settings_security.js
+++ files/assets/js/settings_security.js
@@ -3,7 +3,11 @@ document.getElementById('new_email').addEventListener('input', function () {
 	document.getElementById("email-password-label").classList.remove("d-none");
 	document.getElementById("emailpasswordRequired").classList.remove("d-none");
 });
-const twoStepModal = new bootstrap.Modal(document.getElementById('2faModal'))
+
+document.getElementById('2faToggle').addEventListener('change', function () {
+	const twoStepModal = new bootstrap.Modal(document.getElementById('2faModal'));
+	twoStepModal.show();
+});
 
 function emailVerifyText() {
 	document.getElementById("email-verify-text").innerHTML = "Verification email sent! Please check your inbox.";
