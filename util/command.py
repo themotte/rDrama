74c312a56ec38fcd2969f16d591a3c97384c7e17
Ben Rog-Wilhelm <zorba@pavlovian.net>
2022-11-10 11:32:36 -0600

    A large number of small tooling and README updates.

--- util/command.py
+++ util/command.py
@@ -3,16 +3,15 @@
 import sys
 from common import _operation
 
-def run_test(args):
-    result = _operation("tests", [
+def run_command(argv):
+    result = _operation(f"command",[
         [
             "python3",
-            "-m", "pytest",
-            "-s",
-        ]
+            "-m", "flask",
+        ] + argv[1:]
     ])
 
     sys.exit(result.returncode)
 
 if __name__=='__main__':
-    run_test(sys.argv)
+    run_command(sys.argv)
