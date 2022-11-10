49f958d8a7dd3fca3f560b1b97ab43f513ffb0af
Ben Rog-Wilhelm <zorba@pavlovian.net>
2022-11-10 08:43:22 -0600

    Initial checkin of customizable sqlalchemy-easy-profile.

---
+++ thirdparty/sqlalchemy-easy-profile/README.md
@@ -0,0 +1,141 @@
+# SQLAlchemy Easy Profile
+[![Build Status](https://travis-ci.com/dmvass/sqlalchemy-easy-profile.svg?branch=master)](https://travis-ci.com/dmvass/sqlalchemy-easy-profile)
+[![image](https://img.shields.io/pypi/v/sqlalchemy-easy-profile.svg)](https://pypi.python.org/pypi/sqlalchemy-easy-profile)
+[![codecov](https://codecov.io/gh/dmvass/sqlalchemy-easy-profile/branch/master/graph/badge.svg)](https://codecov.io/gh/dmvass/sqlalchemy-easy-profile)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/dmvass/sqlalchemy-easy-profile/blob/master/LICENSE)
+
+Inspired by [django-querycount](https://github.com/bradmontgomery/django-querycount),
+is a library that hooks into SQLAlchemy to collect metrics, streaming statistics into
+console output and help you understand where in application you have slow or redundant
+queries.
+
+![report example](https://raw.githubusercontent.com/dmvass/sqlalchemy-easy-profile/master/images/report-example.png?raw=true)
+
+## Installation
+Install the package with pip:
+```
+pip install sqlalchemy-easy-profile
+```
+
+## Session profiler
+The profiling session hooks into SQLAlchemy and captures query statements, duration information,
+and query parameters. You also may have multiple profiling sessions active at the same
+time on the same or different Engines. If multiple profiling sessions are active on the
+same engine, queries on that engine will be collected by both sessions and reported on
+different reporters.
+
+You may begin and commit a profiling session as much as you like. Calling begin on an already
+started session or commit on an already committed session will raise an `AssertionError`.
+You also can use a contextmanager interface for session profiling or used it like a decorator.
+This has the effect of only profiling queries occurred within the decorated function or inside
+a manager context.
+
+How to use `begin` and `commit`:
+```python
+from easy_profile import SessionProfiler
+
+profiler = SessionProfiler()
+
+profiler.begin()
+session.query(User).filter(User.name == "Arthur Dent").first()
+profiler.commit()
+
+print(profiler.stats)
+```
+
+How to use as a context manager interface:
+```python
+profiler = SessionProfiler()
+with profiler:
+    session.query(User).filter(User.name == "Arthur Dent").first()
+
+print(profiler.stats)
+```
+
+How to use profiler as a decorator:
+```python
+profiler = SessionProfiler()
+
+class UsersResource:
+    @profiler()
+    def on_get(self, req, resp, **args, **kwargs):
+        return session.query(User).all()
+```
+
+Keep in mind that profiler decorator interface accepts a special reporter and
+If it was not defined by default will be used a base streaming reporter. Decorator
+also accept `name` and `name_callback` optional parameters.
+
+## WSGI integration
+Easy Profiler provides a specified middleware which can prints the number of database
+queries for each HTTP request and can be applied as a WSGI server middleware. So you
+can easily integrate Easy Profiler into any WSGI application.
+
+How to integrate with a Flask application:
+```python
+from flask import Flask
+from easy_profile import EasyProfileMiddleware
+
+app = Flask(__name__)
+app.wsgi_app = EasyProfileMiddleware(app.wsgi_app)
+```
+
+How to integrate with a Falcon application: 
+```python
+import falcon
+from easy_profile import EasyProfileMiddleware
+
+api = application = falcon.API()
+application = EasyProfileMiddleware(application)
+```
+
+## How to customize output
+
+The `StreamReporter` accepts medium-high thresholds, output file destination (stdout by default), a special
+flag for disabling color formatting and number of displayed duplicated queries:
+
+```python
+from flask import Flask
+from easy_profile import EasyProfileMiddleware, StreamReporter
+
+app = Flask(__name__)
+app.wsgi_app = EasyProfileMiddleware(app.wsgi_app, reporter=StreamReporter(display_duplicates=100))
+```
+
+Any custom reporter can be created as:
+
+```python
+from easy_profile.reporters import Reporter
+
+class CustomReporter(Reporter):
+
+    def report(self, path, stats):
+        """Do something with path and stats.
+        
+        :param str path: where profiling occurred
+        :param dict stats: profiling statistics
+
+        """
+        ...
+
+```
+
+## Testing
+To run the tests:
+```
+python setup.py test
+```
+
+Or use `tox` for running in all tests environments.
+
+## License
+This code is distributed under the terms of the MIT license.
+
+## Changes
+A full changelog is maintained in the [CHANGELOG](https://github.com/dmvass/sqlalchemy-easy-profile/blob/master/CHANGELOG.md) file.
+
+## Contributing 
+**sqlalchemy-easy-profile** is an open source project and contributions are
+welcome! Check out the [Issues](https://github.com/dmvass/sqlalchemy-easy-profile/issues)
+page to see if your idea for a contribution has already been mentioned, and feel
+free to raise an issue or submit a pull request.
---
+++ thirdparty/sqlalchemy-easy-profile/easy_profile/profiler.py
@@ -0,0 +1,186 @@
+from collections import Counter, namedtuple, OrderedDict
+import functools
+import inspect
+from queue import Queue
+import re
+import sys
+import time
+
+from sqlalchemy import event
+from sqlalchemy.engine.base import Engine
+
+from .reporters import StreamReporter
+
+# Optimize timer function for the platform
+if sys.platform == "win32":  # pragma: no cover
+    _timer = time.perf_counter
+else:
+    _timer = time.time
+
+
+SQL_OPERATORS = ["select", "insert", "update", "delete"]
+OPERATOR_REGEX = re.compile("(%s) *." % "|".join(SQL_OPERATORS), re.IGNORECASE)
+
+
+def _get_object_name(obj):
+    module = getattr(obj, "__module__", inspect.getmodule(obj).__name__)
+    if hasattr(obj, "__qualname__"):
+        name = obj.__qualname__
+    else:
+        name = obj.__name__
+    return module + "." + name
+
+
+_DebugQuery = namedtuple(
+    "_DebugQuery", "statement,parameters,start_time,end_time"
+)
+
+
+class DebugQuery(_DebugQuery):
+    """Public implementation of the debug query class"""
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
+
+
+class SessionProfiler:
+    """A session profiler for sqlalchemy queries.
+
+    :param Engine engine: sqlalchemy database engine
+
+    :attr bool alive: is True if profiling in progress
+    :attr Queue queries: sqlalchemy queries queue
+
+    """
+
+    _before = "before_cursor_execute"
+    _after = "after_cursor_execute"
+
+    def __init__(self, engine=None):
+        if engine is None:
+            self.engine = Engine
+            self.db_name = "default"
+        else:
+            self.engine = engine
+            self.db_name = engine.url.database or "undefined"
+
+        self.alive = False
+        self.queries = None
+
+        self._stats = None
+
+    def __enter__(self):
+        self.begin()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.commit()
+
+    def __call__(self, path=None, path_callback=None, reporter=None):
+        """Decorate callable object and profile sqlalchemy queries.
+
+        If reporter was not defined by default will be used a base
+        streaming reporter.
+
+        :param easy_profile.reporters.Reporter reporter: profiling reporter
+        :param collections.abc.Callable path_callback: callback for getting
+            more complex path
+
+        """
+        if reporter is None:
+            reporter = StreamReporter()
+
+        def decorator(func):
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                if path_callback is not None:
+                    _path = path_callback(func, *args, **kwargs)
+                else:
+                    _path = path or _get_object_name(func)
+
+                self.begin()
+                try:
+                    result = func(*args, **kwargs)
+                finally:
+                    self.commit()
+                    reporter.report(_path, self.stats)
+                return result
+
+            return wrapper
+
+        return decorator
+
+    @property
+    def stats(self):
+        if self._stats is None:
+            self._reset_stats()
+        return self._stats
+
+    def begin(self):
+        """Begin profiling session.
+
+        :raises AssertionError: When the session is already alive.
+
+        """
+        if self.alive:
+            raise AssertionError("Profiling session has already begun")
+
+        self.alive = True
+        self.queries = Queue()
+        self._reset_stats()
+
+        event.listen(self.engine, self._before, self._before_cursor_execute)
+        event.listen(self.engine, self._after, self._after_cursor_execute)
+
+    def commit(self):
+        """Commit profiling session.
+
+        :raises AssertionError: When the session is not alive.
+
+        """
+        if not self.alive:
+            raise AssertionError("Profiling session is already committed")
+
+        self.alive = False
+        self._get_stats()
+
+        event.remove(self.engine, self._before, self._before_cursor_execute)
+        event.remove(self.engine, self._after, self._after_cursor_execute)
+
+    def _get_stats(self):
+        """Calculate and returns session statistics."""
+        while not self.queries.empty():
+            query = self.queries.get()
+            self._stats["call_stack"].append(query)
+            match = OPERATOR_REGEX.match(query.statement)
+            if match:
+                self._stats[match.group(1).lower()] += 1
+                self._stats["total"] += 1
+                self._stats["duration"] += query.duration
+                duplicates = self._stats["duplicates"].get(query.statement, -1)
+                self._stats["duplicates"][query.statement] = duplicates + 1
+
+        return self._stats
+
+    def _reset_stats(self):
+        self._stats = OrderedDict()
+        self._stats["db"] = self.db_name
+
+        for operator in SQL_OPERATORS:
+            self._stats[operator] = 0
+
+        self._stats["total"] = 0
+        self._stats["duration"] = 0
+        self._stats["call_stack"] = []
+        self._stats["duplicates"] = Counter()
+
+    def _before_cursor_execute(self, conn, cursor, statement, parameters,
+                               context, executemany):
+        context._query_start_time = _timer()
+
+    def _after_cursor_execute(self, conn, cursor, statement, parameters,
+                              context, executemany):
+        self.queries.put(DebugQuery(
+            statement, parameters, context._query_start_time, _timer()
+        ))
