939f671e80484582ea13bd59e3f99b9b135a2d52
Ben Rog-Wilhelm <zorba@pavlovian.net>
2022-11-21 09:38:00 -0600

    Beef up the Production check a little and do true comparison correctly.

--- files/helpers/strings.py
+++ files/helpers/strings.py
@@ -2,3 +2,10 @@
 # clean strings for searching
 def sql_ilike_clean(my_str):
 	return my_str.replace(r'\\', '').replace('_', r'\_').replace('%', '').strip()
+
+def bool_from_string(str_in: str) -> bool:
+	if str_in.lower() in ("yes", "true", "t", "1"):
+		return True
+	if str_in.lower() in ("no", "false", "f", "0"):
+		return False
+	raise ValueError()
