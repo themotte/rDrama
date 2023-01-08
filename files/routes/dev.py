7e910eb01f874ab0ce15ae41d93a2cda0f1ce708
justcool393 <justcool393@gmail.com>
2023-01-08 02:50:34 -0600

    Dev: raise exception if attempted to import outside of debug mode

--- files/routes/dev.py
+++ files/routes/dev.py
@@ -6,6 +6,9 @@ from files.helpers.get import get_user
 from files.helpers.wrappers import admin_level_required
 from files.__main__ import app
 
+if not app.debug:
+	raise ImportError("Importing dev routes is not allowed outside of debug mode!")
+
 @app.post('/dev/sessions/')
 @admin_level_required(PERMS['DEBUG_LOGIN_TO_OTHERS'])
 def login_to_other_account(v):
