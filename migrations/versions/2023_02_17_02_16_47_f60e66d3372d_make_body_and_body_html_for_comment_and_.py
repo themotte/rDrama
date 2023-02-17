9cfcb06eee64705d9940d56138b30734405a7efc
justcool393 <justcool393@gmail.com>
2023-02-16 20:43:31 -0600

    fix migration yet again

--- migrations/versions/2023_02_17_02_16_47_f60e66d3372d_make_body_and_body_html_for_comment_and_.py
+++ migrations/versions/2023_02_17_02_16_47_f60e66d3372d_make_body_and_body_html_for_comment_and_.py
@@ -20,16 +20,16 @@ depends_on = None
 def upgrade():
 	# ### commands adjusted to fix automigration ### #
 	op.alter_column('submissions', 'body', type_=sa.TEXT)
-	op.alter_column('submissions', 'body_html', _type=sa.TEXT)
-	op.alter_column('comments', 'body', _type=sa.TEXT)
-	op.alter_column('comments', 'body_html', _type=sa.TEXT, nullable=False)
+	op.alter_column('submissions', 'body_html', type_=sa.TEXT)
+	op.alter_column('comments', 'body', type_=sa.TEXT)
+	op.alter_column('comments', 'body_html', type_=sa.TEXT, nullable=False)
 	# ### end Alembic commands ###
 
 
 def downgrade():
 	# ### commands adjusted to fix automigration ### #
-	op.alter_column('submissions', 'body', _type=sa.VARCHAR(length=40000))
-	op.alter_column('submissions', 'body_html', _type=sa.VARCHAR(length=40000))
-	op.alter_column('comments', 'body', _type=sa.VARCHAR(length=40000))
-	op.alter_column('comments', 'body_html', _type=sa.VARCHAR(length=40000), nullable=True)
+	op.alter_column('submissions', 'body', type_=sa.VARCHAR(length=40000))
+	op.alter_column('submissions', 'body_html', type_=sa.VARCHAR(length=40000))
+	op.alter_column('comments', 'body', type_=sa.VARCHAR(length=40000))
+	op.alter_column('comments', 'body_html', type_=sa.VARCHAR(length=40000), nullable=True)
 	# ### end Alembic commands ###
