8e11ab8f7d346cc91e76416fe3d393e1643968fc
justcool393 <justcool393@gmail.com>
2023-03-04 15:19:24 -0600

    Add master process warning, docstrings, and clean up formatting

--- files/routes/admin/performance.py
+++ files/routes/admin/performance.py
@@ -11,14 +11,37 @@ from files.helpers.time import format_datetime
 from files.helpers.wrappers import admin_level_required
 from files.__main__ import app
 
-WORKER_PROCESS_NAME: Final[str] = "gunicorn"
+PROCESS_NAME: Final[str] = "gunicorn"
+'''
+The name of the master and worker processes
+'''
+
 INIT_PID: Final[int] = 1
+'''
+The PID of the init process. Used to check an edge case for orphaned workers.
+'''
+
+MEMORY_RSS_WARN_LEVELS_MASTER: dict[int, str] = {
+	0: '',
+	50 * 1024 * 1024: 'text-warn',
+	75 * 1024 * 1024: 'text-danger',
+}
+'''
+Levels to warn for in RAM memory usage for the master process. The master 
+process shouldn't be using much RAM at all since all it basically does is
+orchestrate workers.
+'''
 
 MEMORY_RSS_WARN_LEVELS_WORKER: dict[int, str] = {
 	0: '',
 	200 * 1024 * 1024: 'text-warn',
 	300 * 1024 * 1024: 'text-danger',
 }
+'''
+Levels to warn for in RAM memory usage. There are no warning levels for VM 
+usage because Python seems to intentionally overallocate (presumably to make 
+the interpreter faster) and doesn't tend to touch many of its owned pages.
+'''
 
 @dataclass(frozen=True, slots=True)
 class RenderedPerfInfo:
@@ -45,7 +68,9 @@ class RenderedPerfInfo:
 	@property
 	def memory_rss_css_class(self) -> str:
 		last = ''
-		for mem, css in MEMORY_RSS_WARN_LEVELS_WORKER.items():
+		levels: dict[int, str] = MEMORY_RSS_WARN_LEVELS_MASTER \
+			if self.is_master else MEMORY_RSS_WARN_LEVELS_WORKER
+		for mem, css in levels.items():
 			if self.memory_rss < mem: return last
 			last = css
 		return last
@@ -58,7 +83,9 @@ class RenderedPerfInfo:
 @admin_level_required(PERMS['PERFORMANCE_STATS'])
 def performance_get_stats(v):
 	system_vm = psutil.virtual_memory()
-	processes = {p.pid:RenderedPerfInfo.from_process(p) for p in psutil.process_iter() if p.name() == WORKER_PROCESS_NAME}
+	processes = {p.pid:RenderedPerfInfo.from_process(p) 
+	      for p in psutil.process_iter() 
+		  if p.name() == PROCESS_NAME}
 	return render_template('admin/performance/memory.html', v=v, processes=processes, system_vm=system_vm)
 
 def _signal_master_process(signal:int) -> None:
@@ -67,8 +94,10 @@ def _signal_master_process(signal:int) -> None:
 		abort(500, "This worker is an orphan!")
 	os.kill(ppid, signal)
 
-def _kill_worker_process(pid:int, signal:int) -> None:
-	workers:set[int] = {p.pid for p in psutil.process_iter() if p.name() == WORKER_PROCESS_NAME}
+def _signal_worker_process(pid:int, signal:int) -> None:
+	workers:set[int] = {p.pid 
+		     for p in psutil.process_iter() 
+			 if p.name() == PROCESS_NAME}
 	try:
 		workers.remove(os.getppid()) # don't allow killing the master process
 	except:
@@ -87,13 +116,13 @@ def performance_reload_workers(v):
 @app.post('/performance/workers/<int:pid>/terminate')
 @admin_level_required(PERMS['PERFORMANCE_KILL_PROCESS'])
 def performance_terminate_worker_process(v, pid:int):
-	_kill_worker_process(pid, Signals.SIGTERM)
+	_signal_worker_process(pid, Signals.SIGTERM)
 	return {"message": f"Gracefully shut down worker PID {pid} successfully"}
 
 @app.post('/performance/workers/<int:pid>/kill')
 @admin_level_required(PERMS['PERFORMANCE_KILL_PROCESS'])
 def performance_kill_worker_process(v, pid:int):
-	_kill_worker_process(pid, Signals.SIGKILL)
+	_signal_worker_process(pid, Signals.SIGKILL)
 	return {"message": f"Killed worker with PID {pid} successfully"}
 
 @app.post('/performance/workers/+1')
@@ -101,6 +130,5 @@ def performance_kill_worker_process(v, pid:int):
 @admin_level_required(PERMS['PERFORMANCE_SCALE_UP_DOWN'])
 def performance_scale_up_down(v):
 	scale_up:bool = '+1' in request.url
-	signal:int = Signals.SIGTTIN if scale_up else Signals.SIGTTOU
 	_signal_master_process(Signals.SIGTTIN if scale_up else Signals.SIGTTOU)
 	return {"message": "Sent signal to master to scale " + ("up" if scale_up else "down")}
