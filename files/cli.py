9d088cf93c44e06021043335cdddf7c82d8b4177
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2023-06-11 05:29:47 -0500

    Add volunteer frequency histogram code.

--- files/cli.py
+++ files/cli.py
@@ -5,6 +5,7 @@ from files.__main__ import app
 from files.commands.cron import cron_app_worker
 from files.commands.seed_db import seed_db
 from files.commands.volunteer_janitor_recalc import volunteer_janitor_recalc
+from files.commands.volunteer_janitor_histogram import volunteer_janitor_histogram_cmd
 from files.commands.cron_setup import cron_setup
 import files.classes
 
