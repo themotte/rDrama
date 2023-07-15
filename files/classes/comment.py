365f15eac704f6b08defa2fd63f23268e511d652
Viet Than <thanhoangviet@gmail.com>
2023-07-14 22:35:10 -0400

    makes sure on Comment, reports relationship use created_datetimez

--- files/classes/comment.py
+++ files/classes/comment.py
@@ -71,7 +71,7 @@ class Comment(CreatedBase):
 		viewonly=True)
 	reports = relationship("CommentFlag",
 		primaryjoin="CommentFlag.comment_id == Comment.id",
-		order_by="CommentFlag.created_timestampz",
+		order_by="CommentFlag.created_datetimez",
 		viewonly=True)
 	notes = relationship("UserNote", back_populates="comment")
 
