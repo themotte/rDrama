88c202ba3d3b4e47ce1b1e4784fa462a18abaefc
justcool393 <justcool393@gmail.com>
2023-07-14 22:32:24 -0500

    style: tabify for consistency

--- migrations/versions/2023_06_11_09_09_00_8337558f4f36_move_deleted_utc_into_state_user_.py
+++ migrations/versions/2023_06_11_09_09_00_8337558f4f36_move_deleted_utc_into_state_user_.py
@@ -17,76 +17,76 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('comments', sa.Column('state_user_deleted_utc', sa.DateTime(timezone=True), nullable=True))
-    op.add_column('submissions', sa.Column('state_user_deleted_utc', sa.DateTime(timezone=True), nullable=True))
+	op.add_column('comments', sa.Column('state_user_deleted_utc', sa.DateTime(timezone=True), nullable=True))
+	op.add_column('submissions', sa.Column('state_user_deleted_utc', sa.DateTime(timezone=True), nullable=True))
 
-    op.drop_index('subimssion_binary_group_idx', table_name='submissions')
-    op.drop_index('submission_isdeleted_idx', table_name='submissions')
-    op.drop_index('submission_new_sort_idx', table_name='submissions')
+	op.drop_index('subimssion_binary_group_idx', table_name='submissions')
+	op.drop_index('submission_isdeleted_idx', table_name='submissions')
+	op.drop_index('submission_new_sort_idx', table_name='submissions')
 
-    op.execute("""
-        UPDATE comments 
-        SET state_user_deleted_utc = 
-            CASE 
-                WHEN deleted_utc > 0 THEN 
-                    (timestamp 'epoch' + deleted_utc * interval '1 second') at time zone 'utc' 
-                ELSE NULL 
-            END
-    """)
+	op.execute("""
+		UPDATE comments 
+		SET state_user_deleted_utc = 
+			CASE 
+				WHEN deleted_utc > 0 THEN 
+					(timestamp 'epoch' + deleted_utc * interval '1 second') at time zone 'utc' 
+				ELSE NULL 
+			END
+	""")
 
-    op.execute("""
-        UPDATE submissions 
-        SET state_user_deleted_utc = 
-            CASE 
-                WHEN deleted_utc > 0 THEN 
-                    (timestamp 'epoch' + deleted_utc * interval '1 second') at time zone 'utc' 
-                ELSE NULL 
-            END
-    """)
+	op.execute("""
+		UPDATE submissions 
+		SET state_user_deleted_utc = 
+			CASE 
+				WHEN deleted_utc > 0 THEN 
+					(timestamp 'epoch' + deleted_utc * interval '1 second') at time zone 'utc' 
+				ELSE NULL 
+			END
+	""")
 
-    op.drop_column('comments', 'deleted_utc')
-    op.drop_column('submissions', 'deleted_utc')
+	op.drop_column('comments', 'deleted_utc')
+	op.drop_column('submissions', 'deleted_utc')
 
-    op.create_index('subimssion_binary_group_idx', 'submissions', ['is_banned', 'state_user_deleted_utc', 'over_18'], unique=False)
-    op.create_index('submission_isdeleted_idx', 'submissions', ['state_user_deleted_utc'], unique=False)
-    op.create_index('submission_new_sort_idx', 'submissions', ['is_banned', 'state_user_deleted_utc', sa.text('created_utc DESC'), 'over_18'], unique=False)
+	op.create_index('subimssion_binary_group_idx', 'submissions', ['is_banned', 'state_user_deleted_utc', 'over_18'], unique=False)
+	op.create_index('submission_isdeleted_idx', 'submissions', ['state_user_deleted_utc'], unique=False)
+	op.create_index('submission_new_sort_idx', 'submissions', ['is_banned', 'state_user_deleted_utc', sa.text('created_utc DESC'), 'over_18'], unique=False)
 
 
 
 
 def downgrade():
-    op.add_column('comments', sa.Column('deleted_utc', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=True))
-    op.add_column('submissions', sa.Column('deleted_utc', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=True))
+	op.add_column('comments', sa.Column('deleted_utc', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=True))
+	op.add_column('submissions', sa.Column('deleted_utc', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=True))
 
-    op.drop_index('submission_new_sort_idx', table_name='submissions')
-    op.drop_index('submission_isdeleted_idx', table_name='submissions')
-    op.drop_index('subimssion_binary_group_idx', table_name='submissions')
+	op.drop_index('submission_new_sort_idx', table_name='submissions')
+	op.drop_index('submission_isdeleted_idx', table_name='submissions')
+	op.drop_index('subimssion_binary_group_idx', table_name='submissions')
 
-    op.execute("""
-        UPDATE comments 
-        SET deleted_utc = 
-            COALESCE(
-                EXTRACT(EPOCH FROM state_user_deleted_utc)::integer,
-                0
-            )
-        """)
-    
-    op.execute("""
-        UPDATE submissions 
-        SET deleted_utc = 
-            COALESCE(
-                EXTRACT(EPOCH FROM state_user_deleted_utc)::integer,
-                0
-            )
-        """)
+	op.execute("""
+		UPDATE comments 
+		SET deleted_utc = 
+			COALESCE(
+				EXTRACT(EPOCH FROM state_user_deleted_utc)::integer,
+				0
+			)
+		""")
+	
+	op.execute("""
+		UPDATE submissions 
+		SET deleted_utc = 
+			COALESCE(
+				EXTRACT(EPOCH FROM state_user_deleted_utc)::integer,
+				0
+			)
+		""")
 
-    op.alter_column('comments', 'deleted_utc', nullable=False)
-    op.alter_column('submissions', 'deleted_utc', nullable=False)
+	op.alter_column('comments', 'deleted_utc', nullable=False)
+	op.alter_column('submissions', 'deleted_utc', nullable=False)
 
-    op.drop_column('comments', 'state_user_deleted_utc')
-    op.drop_column('submissions', 'state_user_deleted_utc')
+	op.drop_column('comments', 'state_user_deleted_utc')
+	op.drop_column('submissions', 'state_user_deleted_utc')
 
-    op.create_index('submission_new_sort_idx', 'submissions', ['is_banned', 'deleted_utc', 'created_utc', 'over_18'], unique=False)
-    op.create_index('submission_isdeleted_idx', 'submissions', ['deleted_utc'], unique=False)
-    op.create_index('subimssion_binary_group_idx', 'submissions', ['is_banned', 'deleted_utc', 'over_18'], unique=False)
+	op.create_index('submission_new_sort_idx', 'submissions', ['is_banned', 'deleted_utc', 'created_utc', 'over_18'], unique=False)
+	op.create_index('submission_isdeleted_idx', 'submissions', ['deleted_utc'], unique=False)
+	op.create_index('subimssion_binary_group_idx', 'submissions', ['is_banned', 'deleted_utc', 'over_18'], unique=False)
 
