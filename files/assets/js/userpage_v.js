90186bd99f026b41eec1e185b284803dbce47dcf
justcool393 <justcool393@gmail.com>
2023-07-23 10:35:34 -0500

    Revert "submitFormAjax is different enough from postToast to be annoying"
    This reverts commit 0565e8d6a917dc5d9745146bf922bb3a0fffb8ef.

--- files/assets/js/userpage_v.js
+++ files/assets/js/userpage_v.js
@@ -75,7 +75,6 @@ function transferBux(mobile=false) {
 }
 
 function submitFormAjax(e) {
-	e.preventDefault();
 	document.getElementById('message').classList.add('d-none');
 	document.getElementById('message-mobile').classList.add('d-none');
 	document.getElementById('message-preview').classList.add('d-none');
@@ -83,7 +82,7 @@ function submitFormAjax(e) {
 	
 	const form = e.target;
 	const xhr = new XMLHttpRequest();
-	// TODO: partially combine with postToast?
+	e.preventDefault();
 
 	formData = new FormData(form);
 
@@ -99,9 +98,34 @@ function submitFormAjax(e) {
 	xhr.setRequestHeader('xhr', 'xhr');
 
 	xhr.onload = function() {
-		showToastFromXhr(xhr, null);
+		if (xhr.status >= 200 && xhr.status < 300) {
+			let data = JSON.parse(xhr.response);
+			try {
+				document.getElementById('toast-post-success-text').innerText = data["message"];
+			} catch(e) {
+				document.getElementById('toast-post-success-text').innerText = "Action successful!";
+			}
+			var myToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-success'));
+			myToast.show();
+			return true
+		} else {
+			document.getElementById('toast-post-error-text').innerText = "Error, please try again later."
+			try {
+				let data=JSON.parse(xhr.response);
+				var myToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-error'));
+				myToast.show();
+				if (data && data["error"]) document.getElementById('toast-post-error-text').innerText = data["error"];
+				if (data && data["details"]) document.getElementById('toast-post-error-text').innerText = data["details"];
+			} catch(e) {
+				var myToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-success'));
+				myToast.hide();
+				var myToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-error'));
+				myToast.show();
+			}
+		}
 	};
 
 	xhr.send(formData);
-	return false;
+
+	return false
 }
