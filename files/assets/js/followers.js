0e8856ee463ab9754030613c976fbeb4ed91e809
justcool393 <justcool393@gmail.com>
2023-07-28 03:05:47 -0700

    combine postToast variants (#648)

--- files/assets/js/followers.js
+++ files/assets/js/followers.js
@@ -1,5 +1,5 @@
 function removeFollower(event, username) {
-	post_toast(event.target,'/remove_follow/' + username); 
+	postToastSimple(event.target,'/remove_follow/' + username); 
 	let table = document.getElementById("followers-table");
 	table.removeChild(event.target.parentElement.parentElement);
 }
