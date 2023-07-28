eb78ec61690c00550d90be4379e4ac95917b49ce
Viet Than <thanhoangviet@gmail.com>
2023-07-28 05:41:11 -0500

    🏦 Database Change: convert created utc to datetimez for flags (#633)

---
+++ migrations/versions/2023_07_28_05_33_16_bdb4b7a2e88b_change_created_utc_to_datetimez_for_.py
@@ -0,0 +1,51 @@
+"""Change created_utc to datetimez for flags
+
+Revision ID: bdb4b7a2e88b
+Revises: 55488ee27b00
+Create Date: 2023-07-28 05:33:16.984823+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.functions import now
+
+
+# revision identifiers, used by Alembic.
+revision = 'bdb4b7a2e88b'
+down_revision = '55488ee27b00'
+branch_labels = None
+depends_on = None
+
+
+table_name = 'flags'
+from_column = 'created_utc'
+to_column = 'created_datetimez'
+
+def upgrade():
+    op.add_column(table_name, sa.Column(to_column, sa.DateTime(timezone=True), server_default=now(), nullable=True))
+    op.execute(f"""
+        UPDATE {table_name} 
+        SET {to_column} = 
+            CASE 
+                WHEN {from_column} > 0 THEN 
+                    (timestamp 'epoch' + {from_column} * interval '1 second') at time zone 'utc' 
+                ELSE NULL 
+            END
+    """)
+
+    op.alter_column(table_name, to_column, nullable=False)
+    op.drop_column(table_name, from_column)
+
+
+def downgrade():
+    op.add_column(table_name, sa.Column(from_column, sa.Integer(), server_default=sa.text('0'), nullable=True))
+    op.execute(f"""
+        UPDATE {table_name} 
+        SET {from_column} = 
+            COALESCE(
+                EXTRACT(EPOCH FROM {to_column})::integer,
+                0
+            )
+        """)
+    op.alter_column(table_name, from_column, nullable=False)
+    op.drop_column(table_name, to_column)
