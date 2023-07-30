057bfaec7b673ec7480182fc87ef0e316543b4f2
Viet Than <thanhoangviet@gmail.com>
2023-07-29 22:27:09 -0400

    forgot now

--- migrations/versions/2023_07_30_02_16_18_85dad82a4a67_change_to_datetimez_table_votes.py
+++ migrations/versions/2023_07_30_02_16_18_85dad82a4a67_change_to_datetimez_table_votes.py
@@ -7,6 +7,7 @@ Create Date: 2023-07-30 02:16:18.963489+00:00
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql.functions import now
 
 
 # revision identifiers, used by Alembic.
