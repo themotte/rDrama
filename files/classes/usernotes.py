1827480d18a8c95a4b888c04b4bed0f5d27e1007
justcool393 <justcool393@gmail.com>
2023-08-09 05:33:23 -0500

    Revert "See if we can get around needing a migration for now"
    This reverts commit aade68bfc8f3bc2c4393ad3b553119ba08f1e525.

--- files/classes/usernotes.py
+++ files/classes/usernotes.py
@@ -1,12 +1,10 @@
-from enum import Enum
-
-from sqlalchemy import Enum as EnumType
 from sqlalchemy import *
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy.orm import relationship
 from files.classes.base import CreatedDateTimeBase
 from files.helpers.config.const import *
-
+from enum import Enum
+from sqlalchemy import Enum as EnumType
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class UserTag(Enum):
     Quality = 0
@@ -27,7 +25,7 @@ class UserNote(CreatedDateTimeBase):
 	reference_comment: Mapped[int | None] = mapped_column(Integer, ForeignKey("comments.id", ondelete='SET NULL'))
 	reference_post: Mapped[int | None] = mapped_column(Integer, ForeignKey("submissions.id", ondelete='SET NULL'))
 	note: Mapped[str] = mapped_column(String, nullable=False)
-	tag = mapped_column(EnumType(UserTag), nullable=False) # XXX: Needs migration, see #524
+	tag: Mapped[UserTag] = mapped_column(EnumType(UserTag), nullable=False)
 
 	author = relationship("User", foreign_keys='UserNote.author_id')
 	user = relationship("User", foreign_keys='UserNote.reference_user', back_populates="notes")
