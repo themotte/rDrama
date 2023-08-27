ecd1bbba9405865d7ffd8f62cf857df4fe79225e
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2023-08-27 13:11:49 -0500

    Add a dedicated deployable docker image.

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -4,9 +4,8 @@ set -euxo pipefail
 python3 -m flask db upgrade # this does not actually return error codes properly!
 python3 -m flask cron_setup
 
-cd ./chat
-yarn install
-yarn chat
-cd ..
+if [[ ! -f prebuilt.flag ]]; then
+    ./bootstrap/init_build.sh
+fi
 
 /usr/local/bin/supervisord -c /etc/supervisord.conf
