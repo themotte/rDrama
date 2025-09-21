502fd48b24a7d9a4e76bf1deed98ae04db1e9973
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-09-21 05:21:38 -0500

    Add a single-command util script.

---
+++ util/command.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+from common import _operation
+
+def run_command(argv):
+    result = _operation(f"command", [argv[1:]])
+
+    sys.exit(result.returncode)
+
+if __name__ == '__main__':
+    run_command(sys.argv)
